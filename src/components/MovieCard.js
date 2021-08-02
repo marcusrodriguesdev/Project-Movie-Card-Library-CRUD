bd47639944834ad13448ee4983ad6b36b89be231
Carol_Inglethe <carolinainglethe@gmail.com>
2021-08-02 19:33:44 -0300

    3 - Exiba titulo, sipnose e link de redicerionamento de rota para determinado filme

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import propTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const movieId = `/movies/${movie.id}`;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <h2>Movie Card</h2>
+        <p>{ movie.title }</p>
+        <p>{ movie.storyline }</p>
+        <Link to={ movieId }>VER DETALHES</Link>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: propTypes.objectOf(
+    propTypes.string,
+    propTypes.bool,
+    propTypes.number,
+  ).isRequired,
+};
 
 export default MovieCard;
