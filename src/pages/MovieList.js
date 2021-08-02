ad45402beea2ab76e27ad66cf1f44bb68b77aab3
dineinunes <nunesvieiraclaudinei@outlook.com>
2021-08-02 17:16:03 -0300

    Sluciona requisitos 1 e 2

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,21 +1,41 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
+import Loading from '../components/Loading';
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
+      isLoading: false,
       movies: [],
     };
   }
 
-  render() {
-    const { movies } = this.state;
+  componentDidMount() {
+    this.getMovies();
+  }
 
-    // Render Loading here if the request is still happening
+  async getMovies() {
+    this.setState({ isLoading: true }, async () => {
+      await movieAPI.getMovies()
+        .then((response) => {
+          this.setState({ movies: response }, () => {
+            this.setState({ isLoading: false });
+          });
+        });
+    });
+  }
+
+  render() {
+    const { isLoading, movies } = this.state;
+
+    if (isLoading) {
+      return <Loading />;
+    }
+
+    // { isLoading && <Loading /> }
 
     return (
       <div data-testid="movie-list">
