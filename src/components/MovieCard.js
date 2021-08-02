46a42ffb1c6f017505631e5ffed175408274c0cf
Rafael de Oliveira Romano <rafa-kun@hotmail.com>
2021-08-02 16:23:35 -0300

    :white_check_mark: Requisitos 1 e 2
    Requisito 1 e 2 feitos é necessário corrigir proptypes

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,9 +2,15 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        {console.log(movie)}
+        <h1>{movie.title}</h1>
+        <h2>{movie.subtitle}</h2>
+        <img src={ movie.imagePath } alt="imagens-filmes" />
+        <p>{movie.storyline}</p>
+        <p>{movie.rating}</p>
       </div>
     );
   }
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,24 +1,41 @@
 import React, { Component } from 'react';
+import { Loading } from '../components';
 import MovieCard from '../components/MovieCard';
 
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
       movies: [],
+      loading: true,
     };
   }
 
-  render() {
-    const { movies } = this.state;
+  componentDidMount() {
+    this.FetchApi();
+  }
 
+  async FetchApi() {
+    this.setState({ loading: true },
+      async () => {
+        const responseOfApi = await movieAPI.getMovies();
+        this.setState({
+          loading: false,
+          movies: [...responseOfApi],
+        });
+      });
+  }
+
+  render() {
+    const { movies, loading } = this.state;
     // Render Loading here if the request is still happening
 
     return (
       <div data-testid="movie-list">
+        {loading && <Loading />}
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </div>
     );
