a6b97f61becb5ac1f1f0ebe88bfe127b0513f7ba
Matheus Carvalho <matheuscarvalhoscm@gmail.com>
2021-08-02 17:44:48 -0300

    Requisito 2

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,36 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <img
+          src={ movie.imagePath }
+          alt={ movie.title }
+        />
+        <h2>
+          { movie.title }
+        </h2>
+        <p>
+          { movie.storyline }
+        </p>
+
+        <Link to={ `movies/${movie.id}` }> VER DETALHES </Link>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    imagePath: PropTypes.string,
+    title: PropTypes.string,
+    storyline: PropTypes.string,
+    id: PropTypes.number,
+  }).isRequired,
+};
 
 export default MovieCard;
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
+import { Loading } from '../components';
 
 import * as movieAPI from '../services/movieAPI';
 
@@ -8,17 +9,33 @@ class MovieList extends Component {
     super();
 
     this.state = {
+      loading: true,
       movies: [],
     };
+
+    this.fetchMovies = this.fetchMovies.bind(this);
+  }
+
+  async componentDidMount() {
+    this.fetchMovies();
+  }
+
+  async fetchMovies() {
+    const movieList = await movieAPI.getMovies();
+    this.setState({
+      loading: false,
+      movies: movieList,
+    });
   }
 
   render() {
-    const { movies } = this.state;
+    const { movies, loading } = this.state;
 
     // Render Loading here if the request is still happening
 
     return (
       <div data-testid="movie-list">
+        {loading && <Loading />}
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </div>
     );
