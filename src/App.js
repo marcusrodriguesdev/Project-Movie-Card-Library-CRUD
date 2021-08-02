2afde3fdc2f5b996da8fdd5d60f01d433b452526
Camila Dornas <camiladornas@gmail.com>
2021-08-02 18:24:08 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div>Movie Card Library CRUD</div>
+      <Switch>
+        <Route exact path="/movies/:id" component={ MovieDetails } />
+        <Route exact path="/movies/new" component={ NewMovie } />
+        <Route exact path="/movies/:id/edit" component={ EditMovie } />
+        <Route exact path="/" component={ MovieList } />
+        <Route component={ NotFound } />
+        {/* Pesquisado no site: https://medium.com/@benkissi/how-to-create-a-not-found-page-in-react-95381566271d */}
+      </Switch>
     </Router>
   );
 }
