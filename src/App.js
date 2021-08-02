011d2cddd7434e373453fe9cadea5a1db5806a05
Lucas T Franca <dev.lucasteixeira@gmail.com>
2021-08-02 17:39:39 -0300

    Requisito 5 Finalizado

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import MovieList from "./pages/MovieList";
-import MovieDetails from "./pages/MovieDetails";
-import NewMovie from "./pages/NewMovie";
-import EditMovie from "./pages/EditMovie";
-import NotFound from "./pages/NotFound";
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import NewMovie from './pages/NewMovie';
+import MovieDetails from './pages/MovieDetails';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={MovieList}/>
-        <Route exact path="/movies/new" component={NewMovie}/>
-        <Route exact path="/movies/:id" component={MovieDetails}/>
-        <Route exact path="/movies/:id/edit" component={EditMovie}/>
-        <Route component={NotFound}/>
+        <Route exact path="/" component={ MovieList } />
+        <Route exact path="/movies/new" component={ NewMovie } />
+        <Route exact path="/movies/:id" component={ MovieDetails } />
+        <Route exact path="/movies/:id/edit" component={ EditMovie } />
+        <Route component={ NotFound } />
       </Switch>
     </BrowserRouter>
   );
--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,26 +1,46 @@
 import React, { Component } from 'react';
-
-import { MovieForm } from '../components';
+import PropTypes from 'prop-types';
+import { Redirect } from 'react-router';
+import { Loading, MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
     this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = {
+      status: true,
+      shouldRedirect: false,
+      movie: {},
+    };
+  }
+
+  componentDidMount() {
+    this.fetchMovie();
   }
 
   handleSubmit(updatedMovie) {
+    movieAPI.updateMovie(updatedMovie);
+    this.setState({ shouldRedirect: true });
+  }
+
+  fetchMovie() {
+    this.setState({ status: true }, async () => {
+      const { match } = this.props;
+      const movie = await movieAPI.getMovie(match.params.id);
+      this.setState({ status: false, movie });
+    });
   }
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
-      // Redirect
+      return <Redirect to="/" />;
     }
 
-    if (status === 'loading') {
-      // render Loading
+    if (status) {
+      return <Loading />;
     }
 
     return (
@@ -30,5 +50,9 @@ class EditMovie extends Component {
     );
   }
 }
+
+EditMovie.propTypes = {
+  match: PropTypes.objectOf(PropTypes.object),
+}.isRequired;
 
 export default EditMovie;
