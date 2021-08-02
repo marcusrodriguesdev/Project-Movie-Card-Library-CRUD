4c6aecfe93f6275fb9a13696777c74f517c713c9
alanmdf <alanmoreirascr@gmail.com>
2021-08-02 16:57:22 -0300

    Requisito 4 finalizado

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,22 +1,55 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      movie: {},
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchMovie();
+  }
+
+  async fetchMovie() {
+    const { id } = this.props.match.params;
+    const movieObj = await movieAPI.getMovie(id);
+    this.setState((prevState) => ({
+      loading: !prevState,
+      movie: movieObj,
+    }));
+  }
+
   render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+    const { movie: {
+      id,
+      title,
+      storyline,
+      imagePath,
+      genre,
+      rating,
+      subtitle }, loading } = this.state;
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
-
-    return (
+    if (loading) {
+      return (<Loading />);
+    } return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
+        <p>{ `Title: ${title}` }</p>
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
+        <br />
+        <button type="button"><Link to="/">VOLTAR</Link></button>
+        <button type="button"><Link to={`/movies/${id}/edit`}>EDITAR</Link></button>
       </div>
     );
   }
