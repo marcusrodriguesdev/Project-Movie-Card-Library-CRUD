1848675b458b97c84277536f46c0e1173183b853
gustavoalves23 <gustavo.alves388@gmail.com>
2021-08-02 15:11:31 -0300

    Segundo desafio

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,25 +1,46 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import MovieCard from '../components/MovieCard';
-
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
       movies: [],
+      loading: true,
     };
+  }
+  
+  componentDidMount() {
+    this.fetchMovies();
+  }
+
+  fetchMovies = async () => {
+    movieAPI.getMovies()
+      .then((movies) => {
+        this.setState({
+          movies,
+          loading: false,
+        });
+      });
   }
 
   render() {
-    const { movies } = this.state;
-
+    const { movies, loading } = this.state;
     // Render Loading here if the request is still happening
-
+    const success = (
+      <div>
+        <div id="movie-list">
+          { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
+          <Link id="add-card-link" to="/movies/new">ADICIONAR CARTÃO</Link>
+        </div>
+      </div>
+    );
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        {loading ? <h1>Carregando...</h1> : success }
       </div>
     );
   }
