728148ea648ea4e527bfbb6451e472b49d9aee31
Guilherme C <guilhermedc93@gmail.com>
2021-08-02 16:07:25 -0300

    conclui etapa 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <main>
+      <div>Movie Card Library CRUD</div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/movies/new" component={ NewMovie } />
+          <Route path="/movies/:id/edit" component={ EditMovie } />
+          <Route path="/movies/:id" component={ MovieDetails } />
+          <Route exact path="/" component={ MovieList } />
+          <Route path="/:notfound" component={ NotFound } />
+        </Switch>
+      </BrowserRouter>
+    </main>
   );
 }
 
