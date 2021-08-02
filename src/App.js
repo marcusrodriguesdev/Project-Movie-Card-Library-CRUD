65e2fc04dbd39720fc6f9d6bfc1ba0d877a40989
Matheus Carvalho <matheuscarvalhoscm@gmail.com>
2021-08-02 15:47:08 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter, BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <Router>
-      <div>Movie Card Library CRUD</div>
-    </Router>
-  );
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
+
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Router>
+          <div>Movie Card Library CRUD</div>
+
+          <Switch>
+
+            <Route exact path="/" component={ MovieList } />
+            <Route path="/movies/new" component={ NewMovie } />
+            <Route path="/movies/:id/edit" component={ EditMovie } />
+            <Route path="/movies/:id" component={ MovieDetails } />
+
+            {/* ref para criar essa rota https://ultimatecourses.com/blog/react-router-not-found-component */}
+            <Route component={ NotFound } />
+
+          </Switch>
+
+        </Router>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
