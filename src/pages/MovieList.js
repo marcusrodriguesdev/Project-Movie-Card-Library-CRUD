4f590a81f2e009ee7ae4953eff7df068da88022e
Carol_Inglethe <carolinainglethe@gmail.com>
2021-08-02 18:49:15 -0300

    2 - fazer requisiçao ,buscar e mostrar MovieList

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,25 +1,45 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
 import * as movieAPI from '../services/movieAPI';
+import Loading from '../components/Loading';
 
 class MovieList extends Component {
   constructor() {
     super();
 
+    this.requestMovie = this.requestMovie.bind(this);
+
     this.state = {
       movies: [],
+      loading: true,
     };
   }
 
+  componentDidMount() {
+    this.requestMovie();
+  }
+
+  async requestMovie() {
+    const movies = await movieAPI.getMovies();
+    this.setState({
+      movies: [...movies],
+      loading: false,
+    });
+
+    /* tentativa com fetch(falha)
+    fetch(movieAPI.getMovies())
+      .then((list) => this.setState({ movies: [...list], loading: false })); */
+  }
+
   render() {
-    const { movies } = this.state;
-
+    const { movies, loading } = this.state;
     // Render Loading here if the request is still happening
-
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+
+        {loading ? <Loading className="loading" />
+          : movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+
       </div>
     );
   }
