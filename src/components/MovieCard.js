218838f6fb8ac770f7a2b939de0bcf2e3dc62e68
PedroSpelta <p.h.s.8989@hotmail.com>
2021-08-02 20:47:42 -0300

    finalizando o req 3

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import './moviecard.css';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
     return (
-      <div data-testid="movie-card">
+      <div data-testid="movie-card" className="movie-card">
         Movie Card
+        <div className="movie-title">{this.props.movie.title}</div>
+        <div className="movie-sinopse">{this.props.movie.storyline}</div>
+        <Link to={ `/movies/${this.props.movie.id}` }>VER DETALHES</Link>
       </div>
     );
   }
