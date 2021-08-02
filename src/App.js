abb4ff617fd189b5598d9ce7bdf15dbb39086540
Letícia Castro <aicitelks@gmail.com>
2021-08-02 18:30:15 -0300

    Requisito 1, teste 2: OK

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
 
 export default class App extends React.Component {
   render() {
@@ -10,6 +11,7 @@ export default class App extends React.Component {
       <Router>
         <div>
           <Route path="/" component={ MovieList } />
+          <Route path="/movies/:id" component={ MovieDetails } />
         </div>
       </Router>
     );
