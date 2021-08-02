14d0f9f40ddb858ffe73b2e3cfe7181ecd78ee01
Letícia Castro <aicitelks@gmail.com>
2021-08-02 18:33:44 -0300

    Requisito 1, teste 3: OK

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import './App.css';
 
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 export default class App extends React.Component {
   render() {
@@ -12,6 +15,7 @@ export default class App extends React.Component {
         <div>
           <Route path="/" component={ MovieList } />
           <Route path="/movies/:id" component={ MovieDetails } />
+          <Route path="/movies/new" component={ NewMovie } />
         </div>
       </Router>
     );
