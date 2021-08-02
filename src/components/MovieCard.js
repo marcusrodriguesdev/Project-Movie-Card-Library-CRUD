b7be76a94337e24cc158bd211042d5aa6925de43
Marcos Estevao <dusdoom@gmail.com>
2021-08-02 16:34:06 -0300

    edited structure of <MovieCard />

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes, { string } from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie: { title, subtitle, storyline, id } } = this.props;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <h1>{title}</h1>
+        <h2>{subtitle}</h2>
+        <p>{storyline}</p>
+        <Link to={ `movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.objectOf(PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number,
+    PropTypes.bool,
+  ])).isRequired,
+};
 
 export default MovieCard;
