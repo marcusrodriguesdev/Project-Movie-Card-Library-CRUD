99697a5aeca8317e80b727ebcc23aae8bc653c38
EddieRAM <edusacarneiro@gmail.com>
2021-08-02 20:05:29 -0300

    Requisite 1 correction

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -10,9 +10,9 @@ class EditMovie extends Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  /* handleSubmit(updatedMovie) {
+  handleSubmit(/* updatedMovie */) {
   }
- */
+
   render() {
     const { status, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
