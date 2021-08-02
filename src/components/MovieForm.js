4619ffb01ec42bc3fca0677d401261069a0deb37
Joao Victor <joaogmavictor@gmail.com>
2021-08-02 17:11:24 -0300

    COncluindo Projeto

--- src/components/MovieForm.js
+++ src/components/MovieForm.js
@@ -165,3 +165,8 @@ class MovieForm extends React.Component {
 }
 
 export default MovieForm;
+
+MovieForm.propTypes = {
+  movie: PropTypes.objectOf(String).isRequired,
+  onSubmit: PropTypes.func.isRequired,
+};
