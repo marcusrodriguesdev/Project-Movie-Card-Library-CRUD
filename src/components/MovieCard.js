e1c42558cb2237133dadf757849d9f0eed5cbcf3
Guilherme Gomes <ggs.arquiteto@gmail.com>
2021-08-02 18:13:44 -0300

    resolve o requisito 3 do projeto Movie-cart-library-CRUD

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { id, title, storyline, subtitle, rating, imagePath } = movie;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <h1>{title}</h1>
+        <h3>{subtitle}</h3>
+        <img src={ imagePath } alt={ `imagem do filme ${title}` }/>
+        <p>{storyline}</p>
+        <p>{rating}</p>
+        <Link to={ `/movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }
 }
-
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    id: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
 export default MovieCard;
