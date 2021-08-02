bf1de205a39aca1ec31d92a87ca6da38832886ea
Lucas Caribé <lucasmoc47@gmail.com>
2021-08-02 17:04:50 -0300

    Requisito 4

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,25 +1,72 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
+import * as movieAPI from '../services/movieAPI';
+
 class MovieDetails extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      movie: undefined,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchMovie();
+  }
+
+  fetchMovie = async () => {
+    const {
+      match: {
+        params: { id },
+      },
+    } = this.props;
+
+    const data = await movieAPI.getMovie(Number(id));
+
+    if (data) {
+      this.setState({ movie: data });
+    }
+  };
+
   render() {
     // Change the condition to check the state
     // if (true) return <Loading />;
+    const { movie } = this.state;
+    const { match: { url } } = this.props;
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    if (!movie) {
+      return <Loading />;
+    }
+
+    const { title, imagePath, subtitle, storyline, genre, rating } = movie;
 
     return (
       <div data-testid="movie-details">
+        <p>{title}</p>
         <img alt="Movie Cover" src={ `../${imagePath}` } />
-        <p>{ `Subtitle: ${subtitle}` }</p>
-        <p>{ `Storyline: ${storyline}` }</p>
-        <p>{ `Genre: ${genre}` }</p>
-        <p>{ `Rating: ${rating}` }</p>
+        <p>{`Subtitle: ${subtitle}`}</p>
+        <p>{`Storyline: ${storyline}`}</p>
+        <p>{`Genre: ${genre}`}</p>
+        <p>{`Rating: ${rating}`}</p>
+        <Link to={ `${url}/edit` }>EDITAR</Link>
+        <Link exact to="/">VOLTAR</Link>
       </div>
     );
   }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }),
+    url: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default MovieDetails;
