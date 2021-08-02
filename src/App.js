6e21b0c36b989dd7d9b3a854d45576ff135e01b2
gustavoalves23 <gustavo.alves388@gmail.com>
2021-08-02 14:53:17 -0300

    Primeiro Desafio

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import EditMovie from './pages/EditMovie';
+import MovieDetails from './pages/MovieDetails';
+import MovieList from './pages/MovieList';
+import NewMovie from './pages/NewMovie';
+import NotFound from './pages/NotFound';
+
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={ MovieList } />
+      <Route path="/movies/new" component={ NewMovie } />
+      <Route
+        exact
+        path="/movies/:id"
+        render={ (props) => <MovieDetails { ...props } /> }
+      />
+      <Route path="/movies/:id/edit" render={ (props) => <EditMovie { ...props } /> } />
+      <Route path="/:any" component={ NotFound } />
+    </Switch>
+  </BrowserRouter>
   );
 }
-
-// Comment
 
 export default App;
