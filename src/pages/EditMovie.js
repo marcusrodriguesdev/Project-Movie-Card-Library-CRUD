55a7d428a4680beacbddf3f920a137ccf7d1dd84
gustavoalves23 <gustavo.alves388@gmail.com>
2021-08-02 16:02:01 -0300

    Quinto desafio

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,31 +1,50 @@
 import React, { Component } from 'react';
-
-import { MovieForm } from '../components';
+import { Loading, MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      movie: {},
+      loading: true,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  componentDidMount() {
+    this.fetchMovie();
+  }
+
   handleSubmit(updatedMovie) {
+    const { history } = this.props;
+    movieAPI.updateMovie(updatedMovie)
+      .then(history.push('/'));
+  }
+
+  fetchMovie = async () => {
+    const { match } = this.props;
+    const { params } = match;
+    const { id } = params;
+    movieAPI.getMovie(id)
+      .then((movie) => {
+        this.setState({
+          movie,
+          loading: false,
+        });
+      });
   }
 
   render() {
-    const { status, shouldRedirect, movie } = this.state;
-    if (shouldRedirect) {
-      // Redirect
-    }
-
-    if (status === 'loading') {
-      // render Loading
-    }
-
-    return (
+    const { loading, movie } = this.state;
+    const success = (
       <div data-testid="edit-movie">
         <MovieForm movie={ movie } onSubmit={ this.handleSubmit } />
+      </div>
+    );
+    return (
+      <div>
+        { loading ? <Loading /> : success }
       </div>
     );
   }
