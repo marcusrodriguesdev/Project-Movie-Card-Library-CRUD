fd733951a1bb3fc2684066fd310be8a0655c684b
Matheus Carvalho <matheuscarvalhoscm@gmail.com>
2021-08-02 17:46:49 -0300

    Requisito 3 (ja estava resolvido no ultimo commit mas esqueci de registrar)

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
+
     return (
       <div data-testid="movie-card">
         <img
