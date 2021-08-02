bea6ded42c721eacded967a709c289cadad15bc6
Marcos Estevao <dusdoom@gmail.com>
2021-08-02 15:16:41 -0300

    added all routers to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages/index';
 
 function App() {
   return (
-    <div>
-      Movie Card Library CRUD
-      Commit inicial.
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ MovieList } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route exact path="/movies/:id/edit" component={ EditMovie } />
+        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route path="/:any" component={ NotFound } />
+      </Switch>
+    </BrowserRouter>
 
   );
 }
