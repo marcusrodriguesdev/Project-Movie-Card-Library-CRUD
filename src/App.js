f7766264da6235e1a7100f6cbde0a21b78f28cc1
Lucas T Franca <dev.lucasteixeira@gmail.com>
2021-08-02 14:57:13 -0300

    Requisito 1 Finalizado

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import MovieList from "./pages/MovieList";
+import MovieDetails from "./pages/MovieDetails";
+import NewMovie from "./pages/NewMovie";
+import EditMovie from "./pages/EditMovie";
+import NotFound from "./pages/NotFound";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div>Movie Card Library CRD</div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={MovieList}/>
+        <Route exact path="/movies/new" component={NewMovie}/>
+        <Route exact path="/movies/:id" component={MovieDetails}/>
+        <Route path="/movies/:id/edit" component={EditMovie}/>
+        <Route component={NotFound}/>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
