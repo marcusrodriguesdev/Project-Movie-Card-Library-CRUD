cdc3d55e3488bcb9a3b73e5a7ce4560d544f80d1
Leonardo <leo_mc_inter@hotmail.com>
2021-08-02 16:17:30 -0300

    Criando rotas no App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 import './App.css';
 
 function App() {
   return (
     <Router>
       <div>Movie Card Library CRUD</div>
+      <Switch>
+        <Route path="/">
+          <MovieList />
+        </Route>
+        <Route path="/movies/:id">
+          <MovieDetails />
+        </Route>
+        <Route path="/movies/new">
+          <NewMovie />
+        </Route>
+        <Route path="/movies/:id/edit">
+          <EditMovie />
+        </Route>
+        <Route path="">
+          <NotFound />
+        </Route>
+      </Switch>
     </Router>
   );
 }
