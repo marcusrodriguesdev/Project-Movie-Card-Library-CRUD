8d7454d1fa08a12e5d4845aac0dd2c3866ada684
alanmdf <alanmoreirascr@gmail.com>
2021-08-02 15:14:09 -0300

    Requisito 1 90% concluido

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter, BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import MovieDetails from './pages/MovieDetails';
+import MovieList from './pages/MovieList';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
+
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <div>Movie Card Library CRUD</div>
+      <BrowserRouter>
+        <div>Movie Card Library CRUD</div>
+        <Switch>
+          <Route exact path="/movies/:id/edit" component={ EditMovie } />
+          <Route exact path="/movies/:id" component={ MovieDetails } />
+          <Route exact path="/movies/new" component={ NewMovie } />
+          <Route exact path="/" component={ MovieList } />
+          <Route path="*" component={ NotFound } />
+        </Switch>
+      </BrowserRouter>
     </Router>
   );
 }
