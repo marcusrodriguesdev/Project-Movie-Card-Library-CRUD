2ead95f1fe1175a09fd541faef760a2b65eed68e
GenesisHenriques <genesishenriques7@gmail.com>
2021-08-02 15:42:45 -0300

    Requisito 1 completo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import MoviesList from './pages/MovieList';
+import MoviesDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <BrowserRouter>
+      <div>Movie Card Library CRUD</div>
+
+      <Route exact path="/" component={ MoviesList } />
+      <Route path="/movies/:id" component={ MoviesDetails } />
+      <Route path="/movies/new" component={ NewMovie } />
+      <Route path="/movies/:id/edit" component={ EditMovie } />
+      <Route path="*" component={ NotFound } />
+
+    </BrowserRouter>
   );
 }
 
