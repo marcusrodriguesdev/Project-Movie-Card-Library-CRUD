2205bdb8b93de2dd980c76ea709b64f4894eb082
Miguel <migmenezes.mm2@gmail.com>
2021-08-02 20:10:07 -0300

    req 2

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -2,23 +2,34 @@ import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
 import {Route} from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
+import Loading from '../components/Loading';
+import movies from '../services/movieData';
 
 class MovieList extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      movies: [],
-    };
-  }
-
-  render() {
-    const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
-
-    return (
-      <div data-testid="movie-list">
+  constructor(props) {
+    super(props);
+      this.state = {
+        movies: [],
+        loading:true
+      };
+    }   
+    componentDidMount(){
+      movieAPI.getMovies
+      this.setState({
+        movies:[...movies],
+        loading:false
+      })
+    }
+    
+    
+    render() {
+      const { movies } = this.state;
+      // Render Loading here if the request is still happening
+      
+      
+      return (
+        <div data-testid="movie-list">
+        <Loading />
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </div>
     );
