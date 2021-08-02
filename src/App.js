34d6d397c5a2f378e3f2b499256556ee19639462
Matheus Carvalho <matheuscarvalhoscm@gmail.com>
2021-08-02 15:53:59 -0300

    Pequena refatoraçao requisito 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import MovieList from './pages/MovieList';
@@ -11,24 +11,20 @@ import NotFound from './pages/NotFound';
 class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
-        <Router>
-          <div>Movie Card Library CRUD</div>
+      <Router>
+        <div>Movie Card Library CRUD</div>
 
-          <Switch>
+        <Switch>
 
-            <Route exact path="/" component={ MovieList } />
-            <Route path="/movies/new" component={ NewMovie } />
-            <Route path="/movies/:id/edit" component={ EditMovie } />
-            <Route path="/movies/:id" component={ MovieDetails } />
+          <Route exact path="/" component={ MovieList } />
+          <Route path="/movies/new" component={ NewMovie } />
+          <Route path="/movies/:id/edit" component={ EditMovie } />
+          <Route path="/movies/:id" component={ MovieDetails } />
 
-            {/* ref para criar essa rota https://ultimatecourses.com/blog/react-router-not-found-component */}
-            <Route component={ NotFound } />
-
-          </Switch>
-
-        </Router>
-      </BrowserRouter>
+          {/* ref para criar essa rota https://ultimatecourses.com/blog/react-router-not-found-component */}
+          <Route component={ NotFound } />
+        </Switch>
+      </Router>
     );
   }
 }
