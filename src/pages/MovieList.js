1a84e22e2ab261af66a784470f92c68c5d51840c
Joao Victor <joaogmavictor@gmail.com>
2021-08-02 15:12:47 -0300

    Concluindo Requisito 2

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -4,22 +4,36 @@ import MovieCard from '../components/MovieCard';
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
       movies: [],
     };
+    this.fetchAPi = this.fetchAPi.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchAPi();
+  }
+
+  async fetchAPi() {
+    const fetchMovies = await movieAPI.getMovies();
+    this.setState({ movies: fetchMovies });
   }
 
   render() {
     const { movies } = this.state;
-
+    const Loading = <p> Carregando... </p>;
     // Render Loading here if the request is still happening
 
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        {
+          movies.length === 0
+            ? Loading
+            : movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)
+        }
       </div>
     );
   }
