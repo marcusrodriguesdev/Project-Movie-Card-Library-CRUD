62b0e79c66131e5c7a6efe4e313d924ca756a7bc
Arthur-Jr <arthurjr1906@gmail.com>
2021-08-02 17:43:16 -0300

    Adiciona o segundo requisito.

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -9,17 +9,42 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loading: true,
     };
+
+    this.displayMovies = this.displayMovies.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchApi();
+  }
+
+  displayMovies() {
+    const { movies } = this.state;
+    return (
+      movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)
+    );
+  }
+
+  async fetchApi() {
+    this.setState({ loading: true }, async () => {
+      const response = await movieAPI.getMovies();
+      console.log(response);
+      this.setState({
+        movies: [...response],
+        loading: false,
+      });
+    });
   }
 
   render() {
-    const { movies } = this.state;
-
+    const { loading } = this.state;
+    const loadingDisplay = <span>Carregando...</span>;
     // Render Loading here if the request is still happening
 
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        { loading ? loadingDisplay : this.displayMovies() }
       </div>
     );
   }
