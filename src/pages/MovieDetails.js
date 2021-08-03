91104a9e10b1c36f01dd0cec7f99ff8a5efd3754
Rafael de Oliveira Romano <rafa-kun@hotmail.com>
2021-08-02 21:08:45 -0300

    :white_check_mark: Adicionando exercicio 7
    Adicionando exercicio 7 e 100% do projeto Feliz demais !

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -13,10 +13,16 @@ class MovieDetails extends Component {
       id,
       movieFind: [],
     };
+    this.handleClick = this.handleClick.bind(this);
   }
 
   componentDidMount() {
     this.fetchApiDetails();
+  }
+
+  async handleClick() {
+    const { id } = this.state;
+    await movieAPI.deleteMovie(id);
   }
 
   async fetchApiDetails() {
@@ -50,6 +56,7 @@ class MovieDetails extends Component {
               <p>{ `Rating: ${rating}` }</p>
               <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
               <Link to="/">VOLTAR</Link>
+              <Link to="/" onClick={ this.handleClick }> DELETAR</Link>
             </div>
           )}
       </div>
