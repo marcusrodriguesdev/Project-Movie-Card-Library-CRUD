a8dbc233dffe1d682278663d8a03ae7ca1bc08b3
Camila Dornas <camiladornas@gmail.com>
2021-08-03 19:32:42 -0300

    Corrige erro requisito 1

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 
 import MovieForm from '../components/MovieForm';
-// import * as movieAPI from '../services/movieAPI';
 
 class NewMovie extends Component {
-  constructor(props) {
-    super(props);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.handleSubmit = this.handleSubmit.bind(this);
+  // }
 
   // handleSubmit(newMovie) {
   // }
