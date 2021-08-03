b20f9c818142ca0ecd7e936be1833d71139e3133
PedroSpelta <p.h.s.8989@hotmail.com>
2021-08-02 23:33:18 -0300

    finalizando o req 5

--- src/components/MovieForm.js
+++ src/components/MovieForm.js
@@ -164,4 +164,8 @@ class MovieForm extends React.Component {
   }
 }
 
+// MovieForm.propTypes = {
+//   movie: PropTypes.
+// }
+
 export default MovieForm;
--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,26 +1,41 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { Loading, MovieForm } from '../components';
 
-import { MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { movie: {}, shouldRedirect: false, status: 'loading' };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.fetchMovie(id);
+  }
+
   handleSubmit(updatedMovie) {
+    movieAPI.updateMovie(updatedMovie);
+    this.setState({ shouldRedirect: true });
+  }
+
+  async fetchMovie(id) {
+    const movie = await movieAPI.getMovie(id);
+    this.setState(
+      { movie, status: 'ok' },
+    );
   }
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
-      // Redirect
+      return <Redirect to="/" />;
     }
 
     if (status === 'loading') {
-      // render Loading
+      return <Loading />;
     }
 
     return (
