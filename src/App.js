e4bdb055f4b01d57e5ddad548ffe04c910726951
Gustavo Fernando <gu.bressan@outlook.com>
2021-08-02 19:52:17 -0700

    Requesito 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import MovieList from './pages/MovieList';
+import NotFond from './pages/NotFound';
 
 function App() {
   return (
     <Router>
-      <div>Movie Card Library CRUD</div>
+      <div> Movie Card Library CRUD </div>
+      <Switch>
+        <Route exact path="/movies/new" component={ NewMovie } /> 
+        <Route exact path="/" component={ MovieList } />
+        <Route exact path="/movies/:id" component={ MovieDetails } />
+        <Route exact path="/movies/:id/edit" component={ EditMovie } />
+        <Route exact component={ NotFond } />
+      </Switch>
     </Router>
   );
 }
