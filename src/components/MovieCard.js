cf970b2f91bcdf36348ae3a3f60910ff270a9aec
enioandrade92 <enio.goulart@gmail.com>
2021-08-03 20:07:18 -0300

    Requisito 4

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
@@ -11,10 +12,22 @@ class MovieCard extends React.Component {
         <h2>{ title }</h2>
         <h4>{ subtitle }</h4>
         <h5>{ storyline }</h5>
+        <p>{ rating }</p>
         <Link to={ `/movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    id: PropTypes.number,
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
 
 export default MovieCard;
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,22 +1,49 @@
 import React, { Component } from 'react';
-
-import * as movieAPI from '../services/movieAPI';
+import { Link } from 'react-router-dom';
+import { getMovie } from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
-  render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+  constructor(props) {
+    super(props);
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    this.state = {
+      movie: [],
+      loading: true,
+    };
+
+    this.setStateMovie = this.setStateMovie.bind(this);
+  }
+
+  async componentDidMount() {
+    const { match } = this.props;
+    const { params } = match;
+    const { id } = params;
+    const objMovie = await getMovie(id);
+    this.setStateMovie(objMovie);
+  }
+
+  setStateMovie(objMovie) {
+    this.setState({ movie: objMovie });
+    this.setState({ loading: false });
+  }
+
+  render() {
+    const { loading, movie } = this.state;
+    const { id, genre, imagePath, rating, storyline, subtitle, title } = movie;
 
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
+        <p>{ `Title: ${title}` }</p>
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
+        {loading && <Loading />}
+        <Link to="/">VOLTAR</Link>
+        <br />
+        <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
       </div>
     );
   }
