5345838dd52fb382a639903c43e26dfab9248fdb
Guilherme Viana <guilhermehviana01@gmail.com>
2021-08-03 18:23:18 -0300

    Faz import da página MovieDetails para raiz e define sua rota

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div>Movie Card Library CRUD</div>
-        <Route path="/" component={ MovieList } />
+        <Route exact path="/" component={ MovieList } />
+        <Route path="/movies/:id" render={ () => <MovieDetails /> } />
       </BrowserRouter>
     );
   }
