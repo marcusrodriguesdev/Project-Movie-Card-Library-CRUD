00121493fe810b678fa4b29e5dae52a5058698b0
Guilherme Viana <guilhermehviana01@gmail.com>
2021-08-03 18:27:57 -0300

    Faz import da página EditMovie e define sua rota

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
 import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
         <Route exact path="/" component={ MovieList } />
         <Route path="/movies/:id" render={ () => <MovieDetails /> } />
         <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id/edit" render={ () => <EditMovie /> } />
       </BrowserRouter>
     );
   }
