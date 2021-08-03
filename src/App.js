68cdc722201e0638bb6a3f455915411542dd40f3
Guilherme Viana <guilhermehviana01@gmail.com>
2021-08-03 18:26:07 -0300

    Faz import da página NewMovie e define sua rota

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
         <div>Movie Card Library CRUD</div>
         <Route exact path="/" component={ MovieList } />
         <Route path="/movies/:id" render={ () => <MovieDetails /> } />
+        <Route path="/movies/new" component={ NewMovie } />
       </BrowserRouter>
     );
   }
