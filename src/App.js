267518f75eeeb64efc5982e86ba61c9fbaef8cda
Mariana Garcia <maridgarcia045@gmail.com>
2021-08-03 20:03:14 -0300

    Requisito 6

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
 import NewMovie from './pages/NewMovie';
@@ -10,6 +10,7 @@ import './App.css';
 function App() {
   return (
     <BrowserRouter>
+      <Link to="/movies/new">ADICIONAR CARTÃO </Link>
       <Switch>
         <Route exact path="/" component={ MovieList } />
         <Route exact path="/movies/new" render={ () => <NewMovie /> } />
