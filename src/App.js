19ddc083c9ad9c9be950c9e34191013218442e01
Matheus <matheus.pormanhani@gmail.com>
2021-08-03 08:46:18 -0300

    Correçao req 2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import MovieList from './pages/MovieList';
-import MovieDetails from './pages/MovieDetails';
-import NotFound from './pages/NotFound';
-import NewMovie from './pages/NewMovie';
-import EditMovie from './pages/EditMovie';
+import { MovieList, MovieDetails, NotFound, NewMovie, EditMovie } from './pages';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Switch>
-          <Route path="/" component={ MovieList } />
-          <Route path="/movies/:id" component={ MovieDetails } />
+          <Route exact path="/" component={ MovieList } />
           <Route path="/movies/new" component={ NewMovie } />
           <Route path="/movies/:id/edit" component={ EditMovie } />
-          <Route component={ NotFound } />
+          <Route path="/movies/:id" component={ MovieDetails } />
+          <Route path="*" component={ NotFound } />
         </Switch>
       </BrowserRouter>
     </div>
