ae4142b8d423a66dd7b445bb72709acc75f92c74
Carol_Inglethe <carolinainglethe@gmail.com>
2021-08-02 23:14:05 -0300

    6 - link na pagina inicial para NewMovie, cria novo cartao e retorna p/ inicial

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import MovieCard from '../components/MovieCard';
 import * as movieAPI from '../services/movieAPI';
 import Loading from '../components/Loading';
@@ -40,6 +41,7 @@ class MovieList extends Component {
         {loading ? <Loading className="loading" />
           : movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
 
+        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
       </div>
     );
   }
