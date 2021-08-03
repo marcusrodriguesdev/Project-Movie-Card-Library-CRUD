5a53f429569d3e9ddbcae569c79034ee57605087
Carol_Inglethe <carolinainglethe@gmail.com>
2021-08-02 22:04:01 -0300

    5 - iniciei(incompleto) coloquei texto carregando e validei props do MovieForm

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
 
-import { MovieForm } from '../components';
+import { Loading, MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      status: false,
+      shouldRedirect: false,
+      movie: {},
+    };
+
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit(updatedMovie) {
+    const requestMovie = movieAPI.updateMovie(updatedMovie);
+    this.setState({
+      status: true,
+      movie: requestMovie,
+      shouldRedirect: true,
+    });
   }
 
   render() {
@@ -25,7 +36,9 @@ class EditMovie extends Component {
 
     return (
       <div data-testid="edit-movie">
-        <MovieForm movie={ movie } onSubmit={ this.handleSubmit } />
+
+        { !status ? <Loading />
+          : <MovieForm movie={ movie } onSubmit={ this.handleSubmit } />}
       </div>
     );
   }
