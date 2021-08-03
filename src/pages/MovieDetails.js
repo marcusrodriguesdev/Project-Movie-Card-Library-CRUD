f269f5ff4e5b943e014380537b962908be7e00b9
Matheus <matheus.pormanhani@gmail.com>
2021-08-03 19:14:58 -0300

    Requisito 4

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,27 +1,61 @@
 import React, { Component } from 'react';
-
-// import * as movieAPI from '../services/movieAPI';
-// import { Loading } from '../components';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { getMovie } from '../services/movieAPI';
+import { Loading } from '../components';
 
 class MovieDetails extends Component {
-  render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+  constructor(props) {
+    super(props);
+    const { match } = this.props;
+    const { id } = match.params;
+    this.state = {
+      movie: '',
+      id,
+    };
+  }
 
-    const {
-      // title,
-      storyline, imagePath, genre, rating, subtitle } = {};
+  async componentDidMount() {
+    const { id } = this.state;
+    await getMovie(id).then((movie) => this.setState({
+      movie,
+    }));
+  }
+
+  render() {
+    const { movie } = this.state;
+    const { title, storyline, imagePath, genre, rating, subtitle, id } = movie;
 
     return (
-      <div data-testid="movie-details">
-        <img alt="Movie Cover" src={ `../${imagePath}` } />
-        <p>{ `Subtitle: ${subtitle}` }</p>
-        <p>{ `Storyline: ${storyline}` }</p>
-        <p>{ `Genre: ${genre}` }</p>
-        <p>{ `Rating: ${rating}` }</p>
-      </div>
+      !storyline
+        ? <Loading />
+        : (
+          <div data-testid="movie-details">
+            <img alt="Movie Cover" src={ `../${imagePath}` } />
+            <p>{ `Title: ${title}` }</p>
+            <p>{`Subtitle: ${subtitle}`}</p>
+            <p>{`Storyline: ${storyline}`}</p>
+            <p>{`Genre: ${genre}`}</p>
+            <p>{`Rating: ${rating}`}</p>
+            <Link to={ `/movies/${id}/edit` }>EDITAR </Link>
+            <Link to="/"> VOLTAR </Link>
+          </div>
+        )
     );
   }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    isExact: PropTypes.bool,
+    params: PropTypes.shape({
+      id: PropTypes.string,
+      newentry: PropTypes.string,
+    }),
+    path: PropTypes.string,
+    url: PropTypes.string,
+    newentry: PropTypes.string,
+  }).isRequired,
+};
 
 export default MovieDetails;
