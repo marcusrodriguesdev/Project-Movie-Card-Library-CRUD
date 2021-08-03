e7ba9d85bd7237585995cb30a512cc4b5746aa81
Letícia Castro <aicitelks@gmail.com>
2021-08-03 16:50:09 -0300

    Requisito 2, testes 1 e 2: OK

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Loading } from '../components';
 import MovieCard from '../components/MovieCard';
 
 import * as movieAPI from '../services/movieAPI';
@@ -9,10 +10,12 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loading: true,
     };
   }
 
   componentDidMount() {
+    console.log('componente montado');
     this.fetchGetMovies();
   }
 
@@ -20,18 +23,37 @@ class MovieList extends Component {
   fetchGetMovies = async () => {
     const object = await movieAPI.getMovies();
     this.setState({
-      movies: object
+      loading: false,
+      movies: object,
     });
   }
 
+  toLoad = () => {
+    const { loading } = this.state;
+    if (loading === true) {
+      console.log('carregando...');
+      return (
+        <div>
+          <Loading />
+        </div>
+      );
+    }
+  }
+
   render() {
-    const { movies } = this.state;
+    console.log('componente renderizado');
+    const { movies, loading } = this.state;
 
     // Render Loading here if the request is still happening
+    // this.toLoad();
 
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        { 
+          // como visto na documentação (https://pt-br.reactjs.org/docs/conditional-rendering.html)
+          loading ? <Loading /> :
+          movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)
+        }
       </div>
     );
   }
