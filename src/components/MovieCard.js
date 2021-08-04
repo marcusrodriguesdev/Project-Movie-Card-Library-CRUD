5f80814f28b5b407232ba809437804da9a02270e
Guilherme Giacomini Teixeira <ggiacomini2012@gmail.com>
2021-08-04 15:15:46 -0300

    conclui requisito 5 e deleta o css

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -7,16 +7,14 @@ class MovieCard extends React.Component {
     const { movie } = this.props;
     const { title, storyline, imagePath, genre, rating, subtitle } = movie;
     return (
-      <div className="movie-card-menu">
+      <div data-testid="movie-card">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
-        <div className="movie-card-menu-text">
-          <p>{ `Title: ${title}` }</p>
-          <p>{ `Subtitle: ${subtitle}` }</p>
-          <p>{ `Storyline: ${storyline}` }</p>
-          <p>{ `Genre: ${genre}` }</p>
-          <p>{ `Rating: ${rating}` }</p>
-          <Link className="link" to={ `movies/${movie.id}` }>VER DETALHES</Link>
-        </div>
+        <p>{title}</p>
+        <p>{subtitle}</p>
+        <p>{storyline}</p>
+        <p>{genre}</p>
+        <p>{rating}</p>
+        <Link className="link" to={ `movies/${movie.id}` }>VER DETALHES</Link>
       </div>
     );
   }
