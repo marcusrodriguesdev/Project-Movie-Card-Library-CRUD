d155040a7801f8f5a55a5e35de67860714320870
vitor hugo da silva <vitorhugo.dev1997@gmail.com>
2021-08-04 13:09:23 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div>Movie Card Library CRUD</div>
-    </Router>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/movies/new" render={ () => <NewMovie /> } />
+        <Route path="/movies/:id/edit" render={ () => <EditMovie /> } />
+        <Route path="/movies/:id" render={ () => <MovieDetails /> } />
+        <Route exact path="/" render={ () => <MovieList /> } />
+        <Route path="/*" component={ NotFound } />
+      </Switch>
+    </BrowserRouter>
+    // foi consultado o link: https://imasters.com.br/front-end/gerenciando-rotas-com-react-router, para inplementar a rota que renderiza o componente NotFound.
   );
 }
 
