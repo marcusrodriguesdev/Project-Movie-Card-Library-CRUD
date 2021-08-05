66019a48093880eaf1ab39fd75880800b08ae23e
Letícia Castro <aicitelks@gmail.com>
2021-08-05 15:48:02 -0300

    <MovieDetails /> fazendo a requisição na API: OK

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -4,14 +4,50 @@ import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loading: true,
+      movie: [],
+    };
+  }
+
+  componentDidMount() {
+    this.fetchGetMovie();
+  }
+
+  fetchGetMovie = async () => {
+    const object = await movieAPI.getMovie(1);
+
+    this.setState({
+      loading: false,
+      movie: object,
+    });
+  }
+
+  toLoad() {
+    const { loading } = this.state;
+    if (loading === true) {
+      return(
+        <div>
+          <Loading />
+        </div>
+      )
+    }
+  }
+
   render() {
     // Change the condition to check the state
     // if (true) return <Loading />;
+    this.toLoad();
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    const { movie } = this.state;
+    const { title, storyline, imagePath, genre, rating, subtitle } = movie;
 
     return (
       <div data-testid="movie-details">
+        <h1>Detalhes: </h1>
         <img alt="Movie Cover" src={ `../${imagePath}` } />
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
