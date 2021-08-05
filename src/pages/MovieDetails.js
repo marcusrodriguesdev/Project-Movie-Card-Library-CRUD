ced4edc23dca25e6321c1f3626bc65982d5bedd5
EddieRAM <edusacarneiro@gmail.com>
2021-08-05 13:43:20 -0300

    Requisite 7 done

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
@@ -10,23 +10,37 @@ class MovieDetails extends Component {
     this.state = {
       movie: {},
       isLoading: true,
+      deleteMovie: false,
     };
+    this.handleDeleteMovie = this.handleDeleteMovie.bind(this);
   }
 
   componentDidMount() {
     this.fetchMovieDetails();
   }
 
+  handleDeleteMovie() {
+    const { movie } = this.state;
+    const { id } = movie;
+    movieAPI.deleteMovie(id).then(() => {
+      this.setState({
+        deleteMovie: true,
+      });
+    });
+  }
+
   async fetchMovieDetails() {
-    const { match: { params: { id } } } = this.props; /* https://reactrouter.com/web/api/match */
+    const { match: { params: { id } } } = this.props; /* match https://reactrouter.com/web/api/match */
     const movieData = await movieAPI.getMovie(id);
     this.setState({ movie: movieData, isLoading: false });
   }
 
   render() {
     const { movie: { title, storyline, imagePath, genre, rating, subtitle, id },
-      isLoading } = this.state;
+      isLoading, deleteMovie, shouldRedirect } = this.state;
     if (isLoading) return <Loading />;
+    if (deleteMovie) return <Redirect to="/" />;
+    if (shouldRedirect) return <Redirect exact to="/" />;
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
@@ -37,6 +51,7 @@ class MovieDetails extends Component {
         <p>{ `Rating: ${rating}` }</p>
         <Link to={ `${id}/edit` }>EDITAR</Link>
         <Link to="/">VOLTAR</Link>
+        <Link onClick={ this.handleDeleteMovie } to="/">DELETAR</Link>
       </div>
     );
   }
