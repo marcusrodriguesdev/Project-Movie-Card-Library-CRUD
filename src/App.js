ee5113783e9ca8c8500ea3605e135e10ced5d445
guiiffonseca <guii.ffonseca22@gmail.com>
2021-08-05 17:48:56 -0300

    Requisito 3 sem todos os lints

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
         <Route path="/movies/:id" component={ MovieDetails } />
         <Route path="/movies/new" component={ NewMovie } />
         <Route path="/movies/:id/edit" component={ EditMovie } />
-        <Route component={ NotFound } />
+        <Route path="" component={ NotFound } />
       </switch>
     </BrowserRouter>
   );
