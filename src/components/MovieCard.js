1ea19b4b462e119a57a3f32170b01ede306d7808
Letícia Castro <aicitelks@gmail.com>
2021-08-05 15:55:22 -0300

    <MovieList /> Ajuste no código, remoção da toLoad() e comentários

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -14,9 +14,7 @@ class MovieCard extends React.Component {
             <h1> { title } </h1>
             <h3> { subtitle } </h3>
             <p> { storyline } </p>
-            <p> 
-              { <Link to={`movies/${id}`} > VER DETALHES </Link> }
-            </p>
+            <Link to="/" >VER DETALHES</Link>
           </div>
         </div>
       </div>
