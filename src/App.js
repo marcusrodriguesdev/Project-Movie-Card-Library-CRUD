d2c97522b84001986de9bf688ff0cdc053593366
Letícia Castro <aicitelks@gmail.com>
2021-08-05 18:04:39 -0300

    Correções linter App

--- src/App.js
+++ src/App.js
@@ -13,12 +13,11 @@ export default class App extends React.Component {
     return (
       <Router>
         <Switch>
-          {/* <Route path="/movies/:id" component={ MovieDetails } /> */}
-          <Route path="/movies/new" component={NewMovie} />
-          <Route path="/movies/:id/edit" component={EditMovie} />
-          <Route path="/movies/:id" render={ (props) => <MovieDetails {...props} /> } />
-          <Route path="/" component={MovieList} />
-          <Route path="*" component={NotFound} />
+          <Route path="/movies/new" component={ NewMovie } />
+          <Route path="/movies/:id/edit" component={ EditMovie } />
+          <Route path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
+          <Route path="/" component={ MovieList } />
+          <Route path="*" component={ NotFound } />
         </Switch>
       </Router>
     );
