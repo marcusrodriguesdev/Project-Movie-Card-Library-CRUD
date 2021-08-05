f0a2c8c540946a6d2ed01e1e746cd4c7b3f15afc
Guilherme Giacomini Teixeira <ggiacomini2012@gmail.com>
2021-08-05 12:27:47 -0300

    adiciona css e corrige erros no EsLint refatorado

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -10,11 +10,26 @@ class MovieCard extends React.Component {
       <div data-testid="movie-card" className="movie-card-menu">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
         <div className="movie-card-menu-text">
-          <div>Title: <p>{title}</p></div>
-          <div>Subtitle: <p>{subtitle}</p></div>
-          <div>Storyline: <p>{storyline}</p></div>
-          <div>Genre: <p>{genre}</p></div>
-          <div>Rating: <p>{rating}</p></div>
+          <div>
+            Title:
+            <div>{title}</div>
+          </div>
+          <div>
+            Subtitle:
+            <div>{subtitle}</div>
+          </div>
+          <div>
+            Storyline:
+            <div>{storyline}</div>
+          </div>
+          <div>
+            Genre:
+            <div>{genre}</div>
+          </div>
+          <div>
+            Rating:
+            <div>{rating}</div>
+          </div>
           <Link className="link" to={ `movies/${movie.id}` }>VER DETALHES</Link>
         </div>
       </div>
