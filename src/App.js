009ee008c8fb44c8c59cb2def42eda2c0f23f457
guiiffonseca <guii.ffonseca22@gmail.com>
2021-08-05 16:33:46 -0300

    Requisito 2 sem todos os lints

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
         <Route path="/movies/:id" component={ MovieDetails } />
         <Route path="/movies/new" component={ NewMovie } />
         <Route path="/movies/:id/edit" component={ EditMovie } />
-        <Route path="" component={ NotFound } />
+        <Route component={ NotFound } />
       </switch>
     </BrowserRouter>
   );
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading';
 
 import * as movieAPI from '../services/movieAPI';
 
@@ -9,17 +10,37 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loading: true,
     };
+
+    this.requestMovies = this.requestMovies.bind(this);
+  }
+
+  componentDidMount() {
+    this.requestMovies();
+  }
+
+  async requestMovies() {
+    const returnMovies = await movieAPI.getMovies();
+    this.setState({
+      movies: [...returnMovies],
+      loading: false,
+    });
   }
 
   render() {
-    const { movies } = this.state;
+    const { movies, loading } = this.state;
 
     // Render Loading here if the request is still happening
+    if (loading === true) {
+      return <Loading />;
+    }
 
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        { movies.map((movie) => (
+          <MovieCard key={ movie.title } movie={ movie } />
+        ))}
       </div>
     );
   }
