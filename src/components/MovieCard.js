ee99a30d2645af1ed807759577c15d90ffcf8506
Letícia Castro <aicitelks@gmail.com>
2021-08-05 17:56:24 -0300

    Correções linter <MovieCard />

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -4,17 +4,23 @@ import { Link } from 'react-router-dom';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { id, title, subtitle, storyline, rating, imagePath, bookmarked, genre } = movie;
+    const { id, title, subtitle, storyline, imagePath } = movie;
 
     return (
       <div data-testid="movie-card">
         <div className="theMovieCard">
           <div>
-            <img src={ imagePath } />
-            <h1> { title } </h1>
-            <h3> { subtitle } </h3>
-            <p> { storyline } </p>
-            <Link to={ `/movies/${id}` } >VER DETALHES</Link>
+            <img src={ imagePath } alt='Movie Cover' />
+            <h1>
+              { title }
+            </h1>
+            <h3>
+              { subtitle }
+            </h3>
+            <p>
+              { storyline }
+            </p>
+            <Link to={ `/movies/${id}` }>VER DETALHES</Link>
           </div>
         </div>
       </div>
