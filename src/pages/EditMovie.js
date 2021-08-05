d8ab7e4e74b1c0db023077a5e00a1a7ac971ac96
EddieRAM <edusacarneiro@gmail.com>
2021-08-05 10:32:19 -0300

    Requisite 5:1

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 
-import { MovieForm } from '../components';
+import { Loading, MovieForm } from '../components';
 /* import * as movieAPI from '../services/movieAPI'; */
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      isLoading: true,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
@@ -14,7 +17,8 @@ class EditMovie extends Component {
   }
 
   render() {
-    const { status, shouldRedirect, movie } = this.state;
+    const { isLoading, status, shouldRedirect, movie } = this.state;
+    if (isLoading) return <Loading />;
     if (shouldRedirect) {
       // Redirect
     }
