122f37a2ef0f5f1ecce0dedc10236f4be262d134
Letícia Castro <aicitelks@gmail.com>
2021-08-06 19:58:17 -0300

    Correções linter em <MovieList />

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -37,10 +37,10 @@ class MovieList extends Component {
         {
           // como visto na documentação (https://pt-br.reactjs.org/docs/conditional-rendering.html)
           loading ? <Loading />
-          : <div>
-              <Link to="/movies/new">ADICIONAR CARTÃO</Link>
-              { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
-            </div>
+            : <div>
+                <Link to="/movies/new">ADICIONAR CARTÃO</Link>
+                { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
+              </div>
         }
       </div>
     );
