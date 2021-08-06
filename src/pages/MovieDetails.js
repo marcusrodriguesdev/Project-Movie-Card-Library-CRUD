d878b30fde09a084fb1f64e635ef8fc5b8d4c5b8
Letícia Castro <aicitelks@gmail.com>
2021-08-06 19:56:54 -0300

    Correções linter em <MovieDetails />

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -39,17 +39,18 @@ class MovieDetails extends Component {
     return (
       <div data-testid="movie-details">
         {
-          loading ? <Loading /> : <div>
-            <h1>Detalhes: </h1>
-            <img alt="Movie Cover" src={ `../${ imagePath }` } />
-            <p>{ `Title: ${ title }` }</p>
-            <p>{ `Subtitle: ${ subtitle }` }</p>
-            <p>{ `Storyline: ${ storyline }` }</p>
-            <p>{ `Genre: ${ genre }` }</p>
-            <p>{ `Rating: ${ rating }` }</p>
-            <p><Link to={ `/movies/${ id }/edit` }>EDITAR</Link></p>
-            <p><Link to="/">VOLTAR</Link></p>
-          </div>
+          loading ? <Loading />
+          : <div>
+              <h1>Detalhes: </h1>
+              <img alt="Movie Cover" src={ `../${imagePath}` } />
+              <p>{ `Title: ${title}` }</p>
+              <p>{ `Subtitle: ${subtitle}` }</p>
+              <p>{ `Storyline: ${storyline}` }</p>
+              <p>{ `Genre: ${genre}` }</p>
+              <p>{ `Rating: ${rating}` }</p>
+              <p><Link to={ `/movies/${id}/edit` }>EDITAR</Link></p>
+              <p><Link to="/">VOLTAR</Link></p>
+            </div>
         }
       </div>
     );
