6bb0872b289a4b8793800d4b6b9a5348b925e4de
Henrique Cursino <henriquecursino46@gmail.com>
2021-08-06 09:57:03 -0300

    Requisito 5

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
         <Route exact path="/" component={ MovieList } />
         <Route exact path="/movies/new" component={ NewMovie } />
         <Route exact path="/movies/:id" component={ MovieDetails } />
-        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route path="/movies/:id/edit" render={ (props) => <EditMovie { ...props } /> } />
         <Route component={ NotFound } />
       </Switch>
     </BrowserRouter>
