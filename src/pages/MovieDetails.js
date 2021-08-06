ac66758d6cd9fbb5736d12cb62e7f06f5a4a1515
Matheus Figueiredo <mathhsoares2@live.com>
2021-08-06 16:39:02 -0300

    Requisito 4

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,25 +1,67 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
+import { Link, Redirect } from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor() {
+    super();
+
+    this.handleClick = this.handleClick.bind(this);
+
+    this.state = { redirect: false };
+  }
+
+  componentDidMount() {
+    const p = this.props;
+    const { id } = p.match.params;
+    movieAPI.getMovie(id).then((response) => this.setState({ movie: response }));
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    this.setState({ redirect: true });
+  }
+
   render() {
     // Change the condition to check the state
     // if (true) return <Loading />;
+    let JSX;
+    const s = this.state;
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    if (s.redirect) return <Redirect to="/" />;
+
+    if (s.movie) {
+      const { title, storyline, imagePath, genre, rating, subtitle, id } = s.movie;
+
+      JSX = (
+        <div>
+          <img alt="Movie Cover" src={ `../${imagePath}` } />
+          <p>{ `Title: ${title}` }</p>
+          <p>{ `Subtitle: ${subtitle}` }</p>
+          <p>{ `Storyline: ${storyline}` }</p>
+          <p>{ `Genre: ${genre}` }</p>
+          <p>{ `Rating: ${rating}` }</p>
+          <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+        </div>);
+    } else { JSX = <Loading />; }
 
     return (
+
       <div data-testid="movie-details">
-        <img alt="Movie Cover" src={ `../${imagePath}` } />
-        <p>{ `Subtitle: ${subtitle}` }</p>
-        <p>{ `Storyline: ${storyline}` }</p>
-        <p>{ `Genre: ${genre}` }</p>
-        <p>{ `Rating: ${rating}` }</p>
+        {JSX}
+        <Link to="/">VOLTAR</Link>
       </div>
     );
   }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.objectOf(PropTypes.string).isRequired,
+  }).isRequired,
+};
 
 export default MovieDetails;
