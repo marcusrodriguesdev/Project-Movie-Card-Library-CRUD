6c7ddc52b53ab1c05f05afb70dc5cb4d597dbf81
guiiffonseca <guii.ffonseca22@gmail.com>
2021-08-06 20:42:34 -0300

    Requisito 5

--- src/App.js
+++ src/App.js
@@ -20,9 +20,9 @@ function App() {
       </Router>
       <switch>
         <Route exact path="/" component={ MovieList } />
-        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route exact path="/movies/:id" component={ MovieDetails } />
         <Route path="/movies/new" component={ NewMovie } />
-        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route exact path="/movies/:id/edit" component={ EditMovie } />
         <Route path="" component={ NotFound } />
         <Link to="/movies/new">ADICIONAR CARTÃO</Link>
       </switch>
