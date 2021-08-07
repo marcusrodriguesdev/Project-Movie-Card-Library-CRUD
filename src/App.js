c614b4b077ae5809a3efcb035766ca0a7319deb0
Barros99 <jeanmeneses@hotmail.com>
2021-08-07 12:02:43 -0300

    Corrigindo o page not found com o Switch

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages/index';
 
 function App() {
   return (
     <Router>
-      <span>Movie Card Library CRUD</span>
-      <Route exact path="/" component={ MovieList } />
-      <Route
-        exact
-        path="/movies/:id"
-        component={ MovieDetails }
-      />
+      <div>Movie Card Library CRUD</div>
       <Link to="/movies/new">ADICIONAR CARTÃO</Link>
-      <Route exact path="/movies/new" component={ NewMovie } />
-      <Route exact path="/movies/:id/edit" component={ EditMovie } />
-      <Route component={ NotFound } />
+      <Switch>
+        <Route exact path="/movies/new" component={ NewMovie } />
+        <Route exact path="/" component={ MovieList } />
+        <Route
+          exact
+          path="/movies/:id"
+          component={ MovieDetails }
+        />
+        <Route exact path="/movies/:id/edit" component={ EditMovie } />
+        <Route component={ NotFound } />
+      </Switch>
     </Router>
   );
 }
--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -2,7 +2,21 @@ import React, { Component } from 'react';
 
 class NotFound extends Component {
   render() {
-    return <div data-testid="404-error">Página não encontrada</div>;
+    return (
+      <div data-testid="404-error">
+        <h1>Página não encontrada</h1>
+        <img src="images/chucknorris.png" alt="Not Found" />
+        Image by
+        {' '}
+        <a
+          href="https://dribbble.com/kunchevsky"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Alex Kunchevsky
+        </a>
+      </div>
+    );
   }
 }
 
