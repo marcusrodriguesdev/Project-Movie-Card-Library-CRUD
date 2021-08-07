572f8b3ef2281ff72560020c58d974cc0b6835da
Gustavo <axt872@gmail.com>
2021-08-07 04:06:29 -0300

    Terminei final

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import MovieDetails from './pages/MovieDetails';
 import NewMovie from './pages/NewMovie';
 import EditMovie from './pages/EditMovie';
 import NotFound from './pages/NotFound';
-import DeleteMovie from './pages/DeleteMovie';
 
 function App() {
   return (
@@ -27,11 +26,6 @@ function App() {
           exact
           path="/movies/:id/edit"
           render={ (props) => <EditMovie { ...props } /> }
-        />
-        <Route
-          exact
-          path="/movies/:id/delete"
-          render={ (props) => <DeleteMovie { ...props } /> }
         />
         <Route path="/" component={ NotFound } />
       </Switch>
