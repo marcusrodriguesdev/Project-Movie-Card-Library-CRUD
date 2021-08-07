ac559ee50665e385cb2439ee19ec7d94e9d01089
Barros99 <jeanmeneses@hotmail.com>
2021-08-07 11:10:00 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages/index';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <Router>
+      <span>Movie Card Library CRUD</span>
+      <Route exact path="/" component={ MovieList } />
+      <Route
+        exact
+        path="/movies/:id"
+        component={ MovieDetails }
+      />
+      <Route exact path="/movies/new" component={ NewMovie } />
+      <Route exact path="/movies/:id/edit" component={ EditMovie } />
+      <Route component={ NotFound } />
+    </Router>
   );
 }
 
