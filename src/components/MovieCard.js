da171d367031f4969a54d98ec3d168790af47de9
Demontier <demontier@outlook.com>
2021-08-07 18:38:40 -0300

    Requisito 3

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
     return (
-      <div data-testid="movie-card">
+      <div className="movie-card" data-testid="movie-card">
+        <img className="movie-card-image" src={ movie.imagePath } alt="" />
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{ movie.title }</h4>
+          <h5 className="movie-card-subtitle">{ movie.subtitle }</h5>
+          <p className="movie-card-storyline">{ movie.storyline }</p>
+        </div>
+        <div className="movie-card-rating">
+          <Link to={ `/movies/${(movie.id)}` }>VER DETALHES</Link>
+        </div>
         Movie Card
       </div>
     );
