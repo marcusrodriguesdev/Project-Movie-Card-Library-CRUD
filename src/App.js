740dc3887bdb2e94a1eef84d649e679e7dc5d757
Henrique Lima <hnamix@gmail.com>
2021-08-08 09:20:15 -0400

    lint

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Routers from './components/Routers';
+
 function App() {
   return (
     <BrowserRouter>
