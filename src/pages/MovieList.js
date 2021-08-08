f71b8f6894521fe096e94c11fe8663d72a9fcf1c
Marcio B Cyrilo <m.bruno.cs@hotmail.com>
2021-08-08 19:14:59 -0300

    RQ 2 - Atualizado para MovieList solicitar MovieCard e atulizado teste de Loading para Carregando...

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,26 +1,32 @@
 import React, { Component } from 'react';
+import { Loading } from '../components';
 import MovieCard from '../components/MovieCard';
 
-// import * as movieAPI from '../services/movieAPI';
+import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
     super();
 
     this.state = {
-      movies: [],
+      movies: '',
     };
+  }
+
+  componentDidMount() {
+    movieAPI.getMovies()
+      .then((movies) => this.setState({ movies }));
   }
 
   render() {
     const { movies } = this.state;
 
-    // Render Loading here if the request is still happening
-
     return (
-      <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
-      </div>
+      !movies ? <Loading /> : (
+        <div data-testid="movie-list">
+          {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        </div>
+      )
     );
   }
 }
