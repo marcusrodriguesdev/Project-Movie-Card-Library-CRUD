f73cd408c7b0bb35df5f2f66447211198a262710
Marcus Vinicius Mendes do Nascimento <marcus.mendes89@gmail.com>
2021-08-08 15:22:12 -0300

    Correçoes lint requisito 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
 import NewMovie from './pages/NewMovie';
@@ -10,11 +10,13 @@ function App() {
   return (
     <BrowserRouter>
       <div>Movie Card Library CRUD</div>
-      <Route path="/" component={ MovieList } />
-      <Route path="/movies/:id" component={ MovieDetails } />
-      <Route path="/movies/new" component={ NewMovie } />
-      <Route path="/movies/:id/edit" component={ EditMovie } />
-      <Route path="" component={ NotFound } />
+      <Switch>
+        <Route exact path="/movies/new" component={ NewMovie } />
+        <Route exact path="/movies/:id" component={ MovieDetails } />
+        <Route exact path="/movies/:id/edit" component={ EditMovie } />
+        <Route exact path="/" component={ MovieList } />
+        <Route component={ NotFound } />
+      </Switch>
     </BrowserRouter>
   );
 }
