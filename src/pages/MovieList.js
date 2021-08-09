68c1f6ade3bcda97ba2fec6db01c191072be2a38
Daniel Teixeira <daniel.o.teixeira@gmail.com>
2021-08-08 22:48:43 -0300

    Requisito 6 e 7

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
 import Loading from '../components/Loading';
+import { Link } from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
@@ -31,7 +32,7 @@ class MovieList extends Component {
             ? <Loading />
             : movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)
         }
-
+        <Link to="movies/new">ADICIONAR CARTÃO</Link>
       </div>
     );
   }
