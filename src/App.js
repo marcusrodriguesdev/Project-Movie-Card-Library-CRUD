f741b052cea45fe54bc7f6d3c8d8cb03028ee91d
YasminAraujo3 <yasmin.nunesaraujo@gmail.com>
2021-08-09 12:17:01 -0300

    Requisito 1 feito

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <Router>
       <div>Movie Card Library CRUD</div>
+      <Route path="/movies/:id" component={ MovieDetails } />
+      <Route path="/movies/new" component={ NewMovie } />
+      <Route path="/movies/:id/edit" component={ EditMovie } />
+      <Route exact path="/" component={ MovieList } />
+      <Route exact path="/:undefined" component={ NotFound } />
     </Router>
   );
 }
