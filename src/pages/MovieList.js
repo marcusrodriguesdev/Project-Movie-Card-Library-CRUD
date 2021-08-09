5ac9be4779cc3903895d9654cf0caeb23e79c50a
Natalia Camarinha <natcamarinha@gmail.com>
2021-08-09 11:56:48 -0300

    requisito 2

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading';
 
 import * as movieAPI from '../services/movieAPI';
 
@@ -9,13 +10,28 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loading: true,
     };
   }
 
+  componentDidMount() {
+    this.fetchMovies();
+  }
+
+  fetchMovies = async () => {
+    const data = await movieAPI.getMovies();
+    if (data) {
+      this.setState({ movies: data, loading: false });
+    }
+  }
+
   render() {
-    const { movies } = this.state;
+    const { movies, loading } = this.state;
 
     // Render Loading here if the request is still happening
+    if (loading === true) {
+      return <Loading />;
+    }
 
     return (
       <div data-testid="movie-list">
