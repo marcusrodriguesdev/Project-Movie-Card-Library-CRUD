7aadb7a12a760100d69198d55e4ee985c40bcea8
Henrique Remedios <henriqueremedios@gmail.com>
2021-08-08 21:00:02 -0300

    Requisito 4 feito

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ class App extends React.Component {
       <BrowserRouter>
         <Switch>
           <Route path="/movies/new" component={ NewMovie } />
+          <Route
+            path="/movies/:id/edit"
+            render={ (routeProps) => <EditMovies { ...routeProps } /> }
+          />
           <Route exact path="/movies/:id" component={ MovieDetails } />
           <Route path="/movies/:id/edit" component={ EditMovie } />
           <Route exact path="/" component={ MovieList } />
