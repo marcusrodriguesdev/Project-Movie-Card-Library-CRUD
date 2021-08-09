d22aeca61a67d782fb35814c927a7f347dec2fbd
YasminAraujo3 <yasmin.nunesaraujo@gmail.com>
2021-08-09 12:54:56 -0300

    Requisito 2 feito

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
+import { Loading } from '../components';
 import MovieCard from '../components/MovieCard';
 
-// import * as movieAPI from '../services/movieAPI';
+import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
     super();
 
     this.state = {
-      movies: [],
+      movies: '',
     };
+  }
+
+  componentDidMount() {
+    movieAPI.getMovies()
+      .then((movies) => this.setState({ movies }));
   }
 
   render() {
@@ -18,9 +24,11 @@ class MovieList extends Component {
     // Render Loading here if the request is still happening
 
     return (
-      <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
-      </div>
+      !movies ? <Loading /> : (
+        <div data-testid="movie-list">
+          {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        </div>
+      )
     );
   }
 }
