1e631e552681b13dcd38ade554918c1fdcf158d3
Dororops <leeurw@hotmail.com>
2021-08-09 04:26:59 -0300

    Adicionando Desafio-2 sem lint

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
+import { Loading } from '../components';
 import MovieCard from '../components/MovieCard';
 
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
       movies: [],
     };
   }
 
+  async componentDidMount() {
+    const response = await movieAPI.getMovies();
+    this.setState({
+      movies: response
+    });
+  }
+
   render() {
     const { movies } = this.state;
 
     // Render Loading here if the request is still happening
+    if (movies.length === 0) {
+      return <Loading />;
+    }
 
     return (
       <div data-testid="movie-list">
