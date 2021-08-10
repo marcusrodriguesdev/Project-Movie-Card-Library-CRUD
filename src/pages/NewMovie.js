f9ac97468d0cfcd5485494ccadd7a054ff458a04
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-08-10 03:13:50 -0300

    Edita NewMovie para conter rota de Redirect

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 
 import MovieForm from '../components/MovieForm';
 import * as movieAPI from '../services/movieAPI';
@@ -6,18 +7,33 @@ import * as movieAPI from '../services/movieAPI';
 class NewMovie extends Component {
   constructor(props) {
     super(props);
+
     this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = {
+      redirect: false,
+    };
   }
 
   handleSubmit(newMovie) {
+    movieAPI.createMovie(newMovie);
+
+    this.setState({
+      redirect: true,
+    });
   }
 
   render() {
+    const { redirect } = this.state;
+
     return (
-      <div data-testid="new-movie">
-        <MovieForm onSubmit={ this.handleSubmit } />
-      </div>
+      redirect ? <Redirect to="/" /> : (
+        <div data-testid="new-movie">
+          <MovieForm onSubmit={ this.handleSubmit } />
+        </div>
+      )
     );
   }
 }
+
 export default NewMovie;
