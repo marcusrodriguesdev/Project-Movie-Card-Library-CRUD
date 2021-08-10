2d3bd1c5eafc5b0c7257bd6377caa8451600272c
Mayu <mayusatori@gmail.com>
2021-08-10 08:50:56 -0300

    Requisito 2 resolvido

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <h2>{movie.title}</h2>
+        <img src={ movie.imagePath } alt="imagem" />
+        <h3>{movie.subtitle}</h3>
+        <p>{movie.storyline}</p>
+        <p>{movie.rating}</p>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    imagePath: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+  }).isRequired,
+};
 
 export default MovieCard;
--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Loading } from '../components';
 import MovieCard from '../components/MovieCard';
 
 import * as movieAPI from '../services/movieAPI';
@@ -9,13 +10,33 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loadingScreen: true,
     };
+    this.requisition = this.requisition.bind(this);
+  }
+
+  componentDidMount() {
+    this.requisition();
+  }
+
+  async requisition() {
+    const response = await movieAPI.getMovies();
+    if (response) {
+      this.setState({
+        movies: response,
+        loadingScreen: false,
+      });
+    }
   }
 
   render() {
-    const { movies } = this.state;
+    const { movies, loadingScreen } = this.state;
 
     // Render Loading here if the request is still happening
+
+    if (loadingScreen === true) {
+      return <Loading />;
+    }
 
     return (
       <div data-testid="movie-list">
