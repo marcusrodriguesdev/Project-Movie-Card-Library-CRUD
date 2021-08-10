7b76c1609522e21bcb5ec4955b99c80d7b21f039
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-08-10 01:05:17 -0300

    Corrige identacao

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ function App() {
   return (
     <BrowserRouter>
       <div>Movie Card Library CRUD</div>
-    <Switch>
-      <Route exact path="/" component={ MovieList } />
-      <Route exact path="/movies/:id" component={ MovieDetails } />
-      <Route exact path="/movies/new" component={ NewMovie } />
-      <Route exact path="/movies/:id/edit" component={ EditMovie } />
-      <Route component={ NotFound } />
-    </Switch>
+      <Switch>
+        <Route exact path="/" component={ MovieList } />
+        <Route exact path="/movies/:id" component={ MovieDetails } />
+        <Route exact path="/movies/new" component={ NewMovie } />
+        <Route exact path="/movies/:id/edit" component={ EditMovie } />
+        <Route component={ NotFound } />
+      </Switch>
     </BrowserRouter>
   );
 }
