c40943096d0ff70c5296ed3a81953dbb821ce6df
johntvale <johntvale@gmail.com>
2021-08-10 01:01:19 -0400

    Requisitos 4 e 7

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,25 +1,74 @@
 import React, { Component } from 'react';
 
+import { Link, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+
+    this.state = {
+      movie: {},
+      rendered: true,
+    };
+  }
+
+  componentDidMount() {
+    const { match: { params: { id } } } = this.props;
+    movieAPI.getMovie(id).then((response) => {
+      this.setState({
+        movie: response,
+        rendered: false,
+      });
+    });
+  }
+
+  handleClick() {
+    const { match: { params: { id } } } = this.props;
+    movieAPI.deleteMovie(id)
+      .then(() => {
+        this.setState({
+          backToMain: true,
+        });
+      });
+  }
+
   render() {
     // Change the condition to check the state
     // if (true) return <Loading />;
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    const { movie, rendered, backToMain } = this.state;
+    const { title, storyline, imagePath, genre, rating, subtitle, id } = movie;
 
     return (
       <div data-testid="movie-details">
+        {backToMain && <Redirect to="/" />}
+        {rendered && <Loading />}
+        <p>{ `title: ${title}` }</p>
         <img alt="Movie Cover" src={ `../${imagePath}` } />
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
+        <Link to="/">VOLTAR</Link>
+        <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+        <Link to="/" onClick={ this.handleClick }>DELETAR</Link>
       </div>
     );
   }
 }
+
+// Eu e Guilherme Rodrigues estudamos juntos e recebemos ajuda do Gabriel Lenz gabriellenz-projectMovieCardsCrud.
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.number,
+    }),
+  }).isRequired,
+};
 
 export default MovieDetails;
