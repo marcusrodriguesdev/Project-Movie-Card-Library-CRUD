000d92a43543a1599db43f973682e622df8b06ef
Raphael Rodrigues <raphael_rpc@outlook.com>
2021-08-10 00:09:25 -0300

    entrega do requisito 1 e refaturacao do app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/" render={ () => <MovieList /> } />
-        <Route
-          exact
-          path="/movies/:id"
-          render={ (props) => <MovieDetails { ...props } /> }
-        />
-        <Route exact path="/movies/new" render={ () => <NewMovie /> } />
-        <Route
-          exact
-          path="/movies/:id/edit"
-          render={ (props) => <EditMovie { ...props } /> }
-        />
-        <Route exact path="" render={ () => <NotFound /> } />
-      </Switch>
+      <Route exact path="/" render={ () => <MovieList /> } />
+      <Route
+        exact
+        path="/movies/:id"
+        render={ (props) => <MovieDetails { ...props } /> }
+      />
+      <Route
+        exact
+        path="/movies/:id/edit"
+        render={ (props) => <EditMovie { ...props } /> }
+      />
+      <Route exact path="" render={ () => <NotFound /> } />
+      <Link to="/movies/new">ADICIONAR CARTÃO</Link>
+      <Route exact path="/movies/new" component={ NewMovie } />
+      
     </BrowserRouter>
   );
 }
