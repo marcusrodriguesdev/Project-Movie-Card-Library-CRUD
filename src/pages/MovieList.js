eb0af21ede78a82952f9e90b2cb918e40b8df7e2
Thales <thalessilva.tv@gmail.com>
2021-08-10 12:07:41 -0300

    requisito 2

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import MovieCard from '../components/MovieCard';
-
+import { Loading } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
       movies: [],
+      loadingState: true,
     };
   }
 
-  render() {
-    const { movies } = this.state;
+  componentDidMount() {
+    this.fetchMovies();
+  }
 
-    // Render Loading here if the request is still happening
+  fetchMovies = async () => {
+    const movies = await movieAPI.getMovies();
+    this.setState({
+      movies,
+      loadingState: false,
+    });
+  }
+
+  render() {
+    const { movies, loadingState } = this.state;
 
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        {
+          loadingState ? <Loading /> : movies.map((movie) => (
+            <MovieCard key={ movie.title } movie={ movie } />))
+        }
+        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
       </div>
     );
   }
