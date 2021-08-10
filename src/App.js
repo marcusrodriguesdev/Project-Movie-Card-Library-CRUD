be374accb2cd5f9d4a52c89868c9e2db7802af1b
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-08-10 01:04:12 -0300

    implementa as rotas ultilizando BrowserRouter no componente APP

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Switch, BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <div>Movie Card Library CRUD</div>
-    </Router>
+    <Switch>
+      <Route exact path="/" component={ MovieList } />
+      <Route exact path="/movies/:id" component={ MovieDetails } />
+      <Route exact path="/movies/new" component={ NewMovie } />
+      <Route exact path="/movies/:id/edit" component={ EditMovie } />
+      <Route component={ NotFound } />
+    </Switch>
+    </BrowserRouter>
   );
 }
 
