5b5be7883977e7abc7e6e2d8a9fab7483003e7d7
Guilherme Viana <guilhermehviana01@gmail.com>
2021-08-10 14:07:19 -0300

    teste

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 
 import { MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
@@ -6,17 +7,34 @@ import * as movieAPI from '../services/movieAPI';
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      movie: [],
+      shouldRedirect: false,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
+    // const loading = 'Carregando...';
   }
 
-  handleSubmit(updatedMovie) {
+  // componentDidMount() {
+  // }
+
+  async handleSubmit(updatedMovie) {
+    const updated = await movieAPI.updateMovie(updatedMovie);
+    this.setState({
+      movie: updated,
+      shouldRedirect: true,
+    });
   }
+
+  // async addToForm(id) {
+  // const newMovie = await movieAPI.getMovie(id);
+  // this.setState();
+  // }
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
-    if (shouldRedirect) {
-      // Redirect
+    if (!shouldRedirect) {
+      return <Redirect to="/" />;
     }
 
     if (status === 'loading') {
