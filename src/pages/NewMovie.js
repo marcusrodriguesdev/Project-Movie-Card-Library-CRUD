34ed23cf15a6a739f2014e5036fb4908320b06b2
johntvale <johntvale@gmail.com>
2021-08-10 02:43:29 -0400

    Requisito 6 resolvido

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router';
 
 import MovieForm from '../components/MovieForm';
-// import * as movieAPI from '../services/movieAPI'; importante
+import * as movieAPI from '../services/movieAPI';
 
 class NewMovie extends Component {
   constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      redirect: false,
+    };
   }
 
   handleSubmit(newMovie) {
-    console.log(newMovie);
+    movieAPI.createMovie(newMovie)
+      .then(() => {
+        this.setState({
+          redirect: true,
+        });
+      });
   }
 
   render() {
+    const { redirect } = this.state;
+    if (redirect) return <Redirect to="" />;
     return (
       <div data-testid="new-movie">
         <MovieForm onSubmit={ this.handleSubmit } />
@@ -21,4 +32,7 @@ class NewMovie extends Component {
     );
   }
 }
+
+// Eu e Guilherme Rodrigues estudamos juntos e recebemos ajuda do Gabriel Lenz gabriellenz-projectMovieCardsCrud.
+
 export default NewMovie;
