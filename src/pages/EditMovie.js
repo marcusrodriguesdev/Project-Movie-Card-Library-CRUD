d5d06b2ef3b3fbccf08e41ba1f934937d1673240
GusMazzinghyDev <gusmazzinghy@gmail.com>
2021-08-09 22:36:58 -0300

    6 requisitos

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,26 +1,53 @@
 import React, { Component } from 'react';
-
+import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import { MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    const { match } = this.props;
+    const { params } = match;
+    const { id } = params;
+    this.state = {
+      status: 'loading',
+      id,
+      shouldRedirect: null,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSubmit(updatedMovie) {
+  componentDidMount() {
+    this.carregafilmes();
+  }
+
+  async handleSubmit(updatedMovie) {
+    await movieAPI.updateMovie(updatedMovie);
+    this.setState({ shouldRedirect: '/' });
+  }
+
+  async carregafilmes() {
+    const { id } = this.state;
+    console.log(id);
+    const movie = await movieAPI.getMovie(id);
+    this.setState({
+      movie,
+      status: false,
+    });
+    console.log(movie);
   }
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
-    if (shouldRedirect) {
+    if (shouldRedirect === '/') {
       // Redirect
+      return <Redirect to={ shouldRedirect } />;
     }
 
     if (status === 'loading') {
       // render Loading
+      return <p>Carregando...</p>;
     }
 
     return (
@@ -30,5 +57,14 @@ class EditMovie extends Component {
     );
   }
 }
+
+EditMovie.propTypes = {
+
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    }),
+  }).isRequired,
+};
 
 export default EditMovie;
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,25 +1,66 @@
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      movie: {},
+      loading: true,
+    };
+    this.requiredMovie = this.requiredMovie.bind(this);
+  }
+
+  componentDidMount() {
+    this.loadMovie();
+  }
+
+  async loadMovie() {
+    const { id } = this.state;
+    const movie = await movieAPI.getMovie(id);
+    this.setState({
+      movie,
+      loading: false,
+    });
+  }
+
+  async delete() {
+    const { id } = this.state;
+    await movieAPI.deleteMovie(id);
+  }
+
   render() {
     // Change the condition to check the state
     // if (true) return <Loading />;
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
-
+    const { movie, loading, id } = this.state;
+    const { title, storyline, imagePath, genre, rating, subtitle } = movie;
     return (
-      <div data-testid="movie-details">
-        <img alt="Movie Cover" src={ `../${imagePath}` } />
-        <p>{ `Subtitle: ${subtitle}` }</p>
-        <p>{ `Storyline: ${storyline}` }</p>
-        <p>{ `Genre: ${genre}` }</p>
-        <p>{ `Rating: ${rating}` }</p>
-      </div>
+      <main>
+        <div data-testid="movie-details">
+          <img alt="Movie Cover" src={ `../${imagePath}` } />
+          <p>{ `Title: ${title}` }</p>
+          <p>{ `Subtitle: ${subtitle}` }</p>
+          <p>{ `Storyline: ${storyline}` }</p>
+          <p>{ `Genre: ${genre}` }</p>
+          <p>{ `Rating: ${rating}` }</p>
+        </div>
+        <Link to={ `${id}/edit` }> EDITAR </Link>
+        <Link to="/"> VOLTAR </Link>
+        <Link to="/" onClick={ this.deletar }>DELETAR</Link>
+        <p>{loading ? <Loading /> : '' }</p>
+      </main>
     );
   }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape,
+  params: PropTypes.shape,
+  id: PropTypes.string,
+}.isRequired;
 
 export default MovieDetails;
