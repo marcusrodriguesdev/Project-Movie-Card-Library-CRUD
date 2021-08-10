b265aef0e0317b3ea7cc92608827373289a96965
GusMazzinghyDev <gusmazzinghy@gmail.com>
2021-08-10 00:15:58 -0300

    agora vai

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -4,25 +4,45 @@ import MovieCard from '../components/MovieCard';
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+
+    this.loadMovies = this.loadMovies.bind(this);
 
     this.state = {
       movies: [],
+      loading: true,
     };
   }
 
+  componentDidMount() {
+    this.loadMovies();
+  }
+
+  async loadMovies() {
+    const movies = await movieAPI.getMovies();
+
+    this.setState({
+      movies,
+      loading: false,
+    });
+  }
+
   render() {
-    const { movies } = this.state;
+    const { movies, loading } = this.state;
+    const loadingElement = <span>Carregando...</span>;
 
     // Render Loading here if the request is still happening
 
     return (
-      <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
-      </div>
+      <main>
+        <div data-testid="movie-list">
+          {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        </div>
+        <Link to="/movies/new" movies={ movies }>ADICIONAR CARTÃO</Link>
+        <p>{loading ? loadingElement : '' }</p>
+      </main>
     );
   }
 }
-
 export default MovieList;
