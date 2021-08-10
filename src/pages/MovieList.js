3fda665937d9a796fb3d227c9903d81510602ce1
Matteus Fernandes <matteus_gbi@hotmail.com>
2021-08-10 02:03:02 -0300

    Corrige os erros de lint da page MovieList

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
 import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading';
 
 import * as movieAPI from '../services/movieAPI';
 
@@ -15,36 +18,38 @@ class MovieList extends Component {
       loading: false,
     };
   }
-  
+
   async fetchMovies() {
     this.setState(
       { loading: true },
       async () => {
-        const moviesList = await movieAPI.getMovies();
+        const movies = await movieAPI.getMovies();
         this.setState({
-          movies: moviesList,
+          movies,
           loading: false,
         });
-      });
+      },
+    );
   }
-    
-    renderMovies() {
-      const { movies } = this.state;
-      
-      return (
-        <div data-testid="movie-list">
+
+  renderMovies() {
+    const { movies } = this.state;
+
+    return (
+      <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        <Link to="/movies/new">Adicionar Cartão</Link>
       </div>
     );
   }
-  
+
   render() {
     const { loading } = this.state;
-    
+
     return (
       loading ? <Loading /> : renderMovies()
-      );
-    }
+    );
+  }
 }
 
 export default MovieList;
