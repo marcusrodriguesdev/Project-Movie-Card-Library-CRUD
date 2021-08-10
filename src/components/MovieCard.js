aadd811d8ac8a3df85f415a49083b390046bb2c9
Guilherme <guilhermerg1234@gmail.com>
2021-08-10 03:05:13 -0300

    Test

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-
+// edit
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
