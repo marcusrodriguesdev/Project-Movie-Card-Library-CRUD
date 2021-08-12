2a0bb82fd9fdb32c2f7b6ec33be218da09c08eab
Heitor Santos <heitorg33@gmail.com>
2021-08-11 21:35:58 -0300

    Requisito 4 e 7

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import MovieDetails from './pages/MovieDetails';
 import NewMovie from './pages/NewMovie';
 import EditMovie from './pages/EditMovie';
@@ -11,11 +11,13 @@ function App() {
     <BrowserRouter>
       <div>
         Movie Card Library CRUD
-        <Route path="/" component={ MovieList } />
-        <Route path="/movies/:id" component={ MovieDetails } />
-        <Route path="/movies/new" component={ NewMovie } />
-        <Route path="/movies/:id/edit" component={ EditMovie } />
-        <Route path="" component={ NotFound } />
+        {/* <Switch> */}
+        <Route exact path="/" component={ MovieList } />
+        <Route exact path="/movies/:id" component={ MovieDetails } />
+        <Route exact path="/movies/new" component={ NewMovie } />
+        <Route exact path="/movies/:id/edit" component={ EditMovie } />
+        <Route component={ NotFound } />
+        {/* </Switch> */}
       </div>
     </BrowserRouter>
   );
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,25 +1,68 @@
-import React, { Component } from 'react';
-
+import React from 'react';
+import PropyTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
-class MovieDetails extends Component {
+class MovieDetails extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      movie: {},
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    // movieAPI.getMovie().then((movie) => this.setState({  }));
+    this.fetchMovie();
+  }
+
+  fetchMovie = async () => {
+    const {
+      match: {
+        params: { id },
+      },
+    } = this.props;
+    const movie = await movieAPI.getMovie(id);
+    this.setState({
+      movie,
+      loading: false,
+    });
+  };
+
   render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+    const { movie, loading } = this.state;
+    const { title, storyline, imagePath, genre, rating, subtitle, id } = movie;
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
-
+    if (loading) {
+      return <Loading />;
+    }
+    const del = () => { movieAPI.deleteMovie(id); };
+    console.log(this.props);
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
-        <p>{ `Subtitle: ${subtitle}` }</p>
-        <p>{ `Storyline: ${storyline}` }</p>
-        <p>{ `Genre: ${genre}` }</p>
-        <p>{ `Rating: ${rating}` }</p>
+        <h2>{`Title: ${title}`}</h2>
+        <p>{`Subtitle: ${subtitle}`}</p>
+        <p>{`Storyline: ${storyline}`}</p>
+        <p>{`Genre: ${genre}`}</p>
+        <p>{`Rating: ${rating}`}</p>
+        <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+        <Link to="/">VOLTAR</Link>
+        <Link to="/" onClick={ del }>DELETAR</Link>
       </div>
     );
   }
 }
+
+MovieDetails.propyTypes = {
+  match: PropyTypes.shape({
+    params: PropyTypes.shape({
+      id: PropyTypes.number.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
 
 export default MovieDetails;
