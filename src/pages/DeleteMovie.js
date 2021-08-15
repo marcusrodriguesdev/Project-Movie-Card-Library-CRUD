a81aac5221854d02475025adeaefe1cc39dfc0e2
Luishsk93 <hyungsunkangkr@gmail.com>
2021-08-15 20:23:26 -0300

    Requisito 7 - Correcao do Proptypes

---
+++ src/pages/DeleteMovie.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import * as movieAPI from '../services/movieAPI';
+
+class DeleteMovie extends Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      shouldRedirect: false,
+    };
+  }
+
+  handleSubmit() {
+    const { match: { params: { id } } } = this.props;
+    movieAPI.deleteMovie(id);
+    this.setState({ shouldRedirect: true });
+  }
+
+  render() {
+    const { shouldRedirect } = this.state;
+    if (shouldRedirect) {
+      return <Redirect to="/" />;
+    }
+    return (
+      <div>
+        {this.handleSubmit()}
+      </div>
+    );
+  }
+}
+
+DeleteMovie.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    }),
+  }).isRequired,
+};
+
+export default DeleteMovie;
