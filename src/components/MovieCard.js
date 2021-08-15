024384578d1d4bbc3ccda7f221ec64158658349d
Alef Sloan <Alef_sloan@hotmail.com>
2021-08-15 03:42:41 -0300

    Requisito 3 finalizado - Lint com erros

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { imagePath, title, storyline, id } = movie;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <div>
+          <img src={ imagePath } alt={ title } />
+          <h3>{ title }</h3>
+        </div>
+        <div>
+          <p>{ storyline }</p>
+        </div>
+        <div>
+          <Link to={ `/movies/${id}` }>VER DETALHES</Link>
+        </div>
       </div>
     );
   }
