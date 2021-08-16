28c9413c91d80b667069ac91d2ea205f743b990a
Thiago <thiagodesouza.dev@gmail.com>
2021-08-16 11:28:20 -0300

    requisito 1 Renderize BrowserRouter no componente App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div>Movie Card Library CRUD</div>
-    </Router>
+    <Switch>
+      <Route path="/movies/:id/edit" component={ EditMovie } />
+      <Route exact path="/movies/new" component={ NewMovie } />
+      <Route exact path="/" component={ MovieList } />
+      <Route path="/movies/:id" component={ MovieDetails } />
+      <Route path="*" component={ NotFound } />
+    </Switch>
   );
 }
 
