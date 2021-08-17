45d1e8fa25cee3a315e0ee137ed16e4bce918a3a
Thiago <thiagodesouza.dev@gmail.com>
2021-08-16 23:56:22 -0300

    fix lint

--- src/components/MovieForm.js
+++ src/components/MovieForm.js
@@ -164,4 +164,17 @@ class MovieForm extends React.Component {
   }
 }
 
+MovieForm.propTypes = {
+  movie: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    imagePath: PropTypes.string,
+    bookmarked: PropTypes.bool,
+    genre: PropTypes.string,
+  })).isRequired,
+  onSubmit: PropTypes.func.isRequired,
+};
+
 export default MovieForm;
