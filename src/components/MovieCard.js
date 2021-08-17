6c74c89e268eb1273f6398008ae58532fb8cdb75
Lucas L. S. Haine <lucaslshaine@gmail.com>
2021-08-17 12:00:19 -0300

    Informações necessárias de cada filme renderizadas na página.

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <div style={ {color: 'red'} }>{ movie.title }</div>
+        <div>{ movie.storyline }</div>
+        <a href={ `http://localhost/movies/${movie.id}` }>VER DETALHES</a>
       </div>
     );
   }
