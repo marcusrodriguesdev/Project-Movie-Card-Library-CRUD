3b00dbb0b5db2d4c25adf20639258eafc59f195b
Lucas L. S. Haine <lucaslshaine@gmail.com>
2021-08-17 11:59:41 -0300

    Requisição à API feita com sucesso.

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,28 +1,48 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
+// import { getMovies } from '../services/MovieAPI';
 
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
     super();
-
     this.state = {
       movies: [],
+      isLoading: true,
     };
+
+    this.fetchMovies = this.fetchMovies.bind(this);
+  }
+
+  async fetchMovies(){
+    const movies = await movieAPI.getMovies();
+    this.setState({ movies, isLoading: false });
+  }
+
+  componentDidMount() {
+    this.fetchMovies();
   }
 
   render() {
-    const { movies } = this.state;
-
+    const { movies, isLoading } = this.state;
     // Render Loading here if the request is still happening
+    // movies.push(movieAPI.getMovies());
+    // getMovies();
+
+    if (isLoading) {
+      return (
+        <p>Carregando...</p>
+      );
+    }
 
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        { movies.map((movie) => <MovieCard key={ movie.id } movie={ movie } />) }
       </div>
     );
   }
 }
 
+// {movies.map((movie) => <MovieCard key={ movie.id } movie={ movie } />)}
 export default MovieList;
