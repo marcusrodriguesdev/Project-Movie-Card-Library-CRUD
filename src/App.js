790b5ceb20bebfa4f213b82a0c520fb71ed33862
Alexander <alexanderteodorocastro95@hotmail.com>
2021-08-23 00:24:39 -0300

    requisito 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages/index';
 
 function App() {
   return (
-    <div>Iniciando agora !</div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ MovieList } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route exact path="/movies/:id/edit" component={ EditMovie } />
+        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route path="*" component={ NotFound } />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
 
 import * as movieAPI from '../services/movieAPI';
+import Loading from '../components/Loading';
 
 class MovieList extends Component {
   constructor() {
@@ -12,10 +13,25 @@ class MovieList extends Component {
     };
   }
 
+  async componentDidMount() {
+    const searchMovies = await movieAPI.getMovies();
+    this.handleFetch(searchMovies);
+  }
+
+  handleFetch(request) {
+    this.setState({
+      movies: [...request],
+    });
+  }
+
   render() {
     const { movies } = this.state;
 
-    // Render Loading here if the request is still happening
+    if (movies.length === 0) {
+      return (
+        <Loading />
+      );
+    }
 
     return (
       <div data-testid="movie-list">
