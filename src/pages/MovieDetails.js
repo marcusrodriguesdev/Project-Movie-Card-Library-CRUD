1cb5af45818f4af7b9fc9ce5bd32ccb57a96ed4b
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2021-08-24 11:34:13 -0300

    Botões VOLTAR e EDITAR adicionados

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
@@ -32,15 +33,19 @@ class MovieDetails extends Component {
     }
 
     const { title, storyline, imagePath, genre, rating, subtitle } = this.state.movie;
+    const { id } = this.props.match.params;
 
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
-        <p>{ `String: ${Object.keys(this.props)}` }</p>
+        <p>{ `Title: ${title}` }</p>
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
+        <button><Link to="/">VOLTAR</Link></button>
+        <br />
+        <button><Link to={ `/movies/${id}/edit` }>EDITAR</Link></button>
       </div>
     );
   }
