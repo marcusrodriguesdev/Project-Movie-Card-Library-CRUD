b641acb378e636e334945f4072165cf4dda4b5b9
Alexsander-Trybe <Alexsander81002745@gmail.com>
2021-08-28 19:05:08 -0300

    Commit test

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { key, movie } = this.props;
+    const { id, title, imagePath, subtitle, storyline } = movie;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <img src={ imagePath } alt={ imagePath } />
+        <div key={ key }>
+          <p>{ title }</p>
+          <p>{ subtitle }</p>
+          <p>{ storyline }</p>
+          <Link to={ `/movies/${id}` }>
+            VER DETALHES
+          </Link>
+        </div>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  key: PropTypes.string.isRequired,
+  movie: PropTypes.objectOf(PropTypes.string).isRequired,
+};
 
 export default MovieCard;
