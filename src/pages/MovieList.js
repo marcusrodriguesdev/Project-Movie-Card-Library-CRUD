e849d1d2783bff21cc7ddd2f9d68f52f8cfd29d1
Alexsander-Trybe <Alexsander81002745@gmail.com>
2021-08-28 18:06:28 -0300

    Requisito 3 finalizado

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import Loading from '../components/Loading';
 import MovieCard from '../components/MovieCard';
 
-import * as movieAPI from '../services/movieAPI';
+import { getMovies } from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
@@ -9,13 +10,24 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loading: true,
     };
   }
 
+  getMovieApi = async () => {
+    const moviesApi = await getMovies();
+    this.setState({ loading: false, movies: moviesApi });
+  }
+
+  componentDidMount = () => {
+    this.getMovieApi();
+  }
+
   render() {
-    const { movies } = this.state;
+    const { movies, loading } = this.state;
 
     // Render Loading here if the request is still happening
+    if (loading) return <Loading />;
 
     return (
       <div data-testid="movie-list">
