1c39f3cbdf1a24adc99b89b0474ed8a3b6503ff9
Itaopara Neto <itaopara@gmail.com>
2021-08-31 18:39:52 -0300

    test

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 class Loading extends Component {
   render() {
     return (
-      <div>Loading message</div>
+      <h1>
+        Carregando...
+      </h1>
     );
   }
 }
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import { Loading } from '../components';
 import MovieCard from '../components/MovieCard';
 
-import * as movieAPI from '../services/movieAPI';
+import { getMovies } from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
@@ -9,17 +10,32 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loading: true,
     };
+    this.changeState = this.changeState.bind(this);
+  }
+
+  componentDidMount() {
+    this.changeState();
+  }
+
+  changeState() {
+    console.log('alo');
+    this.setState({
+      loading: false,
+    });
   }
 
   render() {
-    const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
+    const { movies, loading } = this.state;
 
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        {loading === true
+          ? <Loading />
+          : movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        {/* <Loading /> */}
+        {/* {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)} */}
       </div>
     );
   }
