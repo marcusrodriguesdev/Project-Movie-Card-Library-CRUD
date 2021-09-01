23ee3a194a9cbea99047d4af11160fb44301b615
paulrpn <paulrpn@live.com>
2021-09-01 16:10:10 -0300

    requisito 3 + linter

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, /* subtitle, */ storyline, /* rating, */ imagePath } = movie;
+    const { id, title, storyline, imagePath } = movie;
     return (
       <div className="movie-card" data-testid="movie-card">
         <img alt="Movie Cover" className="movie-card-image" src={ imagePath } />
         <div className="movie-card-body">
-          <h4 data-testid="movie-card-title" className="movie-card-title">{title}</h4>
-          <p className="movie-card-storyline">{storyline}</p>
+          <h4 data-testid="movie-card-title" className="movie-card-title">
+            {title}
+          </h4>
+          <p className="movie-card-storyline">
+            {storyline}
+          </p>
         </div>
-        <p className="detail">VER DETALHES</p>
-        {/* <Rating rating={ parseFloat(rating) } /> */}
+        <p className="detail-card">
+          <Link
+            style={
+              { textDecoration: 'none', color: 'rgb(247, 153, 14)' }
+            }
+            to={ `/movies/${id}` }
+          >
+            VER DETALHES
+          </Link>
+        </p>
       </div>
     );
   }
@@ -21,6 +34,7 @@ class MovieCard extends React.Component {
 
 MovieCard.propTypes = {
   movie: PropTypes.shape({
+    id: PropTypes.number,
     title: PropTypes.string,
     storyline: PropTypes.string,
     imagePath: PropTypes.string,
