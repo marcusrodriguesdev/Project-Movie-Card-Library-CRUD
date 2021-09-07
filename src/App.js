df0885c5183539f741903d6a4d13d0df508ad8fa
Bernardo Raffide <bernardoraffide@pop-os.localdomain>
2021-09-07 15:11:40 -0300

    agr acho q vai mesmo o requisito 1

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ function App() {
       <BrowserRouter>
         <Switch>
           <Route path="/" exact component={ MovieList } />
-          <Route path="/movies/:id" component={ MovieDetails } />
-          <Route path="/movies/new" component={ NewMovie } />
-          <Route path="/movies/:id/edit" component={ EditMovie } />
+          <Route path="/movies/new" exact component={ NewMovie } />
+          <Route path="/movies/:id" exact component={ MovieDetails } />
+          <Route path="/movies/:id/edit" exact component={ EditMovie } />
           <Route path="*" component={ NotFound } />
         </Switch>
       </BrowserRouter>
