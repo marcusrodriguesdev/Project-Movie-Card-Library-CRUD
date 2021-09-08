ef3379a85e2b68c6775327cc37e6cab7e79fba88
Rafael Bamberg <raafaelbamberg@gmail.com>
2021-09-08 06:55:12 -0300

    config lint fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import MovieList from './pages/MovieList'
 import MovieDetails from './pages/MovieDetails';
 import EditMovie from './pages/MovieList';
-import NewMovie from './pages/NewMovie'
-import NotFound from './pages/NotFound'
+import NewMovie from './pages/NewMovie';
+import NotFound from './pages/NotFound';
 
 
 function App() {
   return (
-    <BrowserRouter>
-    <div>Movie Card Library CRUD</div>
-    <Switch>
-    <Route exact path="/" component={ MovieList } />
+      <BrowserRouter>
+      <h1>Movie Card Library CRUD</h1>
+      <Switch>
+        <Route exact path="/" component={ MovieList } />
         <Route path="/movies/new" component={ NewMovie } />
-        <Route path="/movies/:id" component={ MovieDetails } />
         <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route path="/movies/:id" component={ MovieDetails } />
         <Route path="*" component={ NotFound } />
-    </Switch>
+      </Switch>
     </BrowserRouter>
   );
 }
 
 export default App;
+
+// aaaa
