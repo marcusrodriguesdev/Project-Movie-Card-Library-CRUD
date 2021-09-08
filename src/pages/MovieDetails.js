99afe7bceeee23866c912eb73352638582fe3ca1
Rafael Bamberg <raafaelbamberg@gmail.com>
2021-09-08 06:37:48 -0300

    requisito 1 a 4

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,25 +1,57 @@
 import React, { Component } from 'react';
-
+import propTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
-  render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+  constructor() {
+    super();
+    this.state = {
+      getDetails: {},
+      loading: true,
+    };
+    this.movDetails = this.movDetails.bind(this);
+  }
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+  componentDidMount() {
+    this.movDetails();
+  }
+
+  async movDetails() {
+    const { match: { params: { id } } } = this.props;
+    const getDetails = await movieAPI.getMovie(id);
+    this.setState({ getDetails, loading: false });
+  }
+
+  render() {
+    const { getDetails, loading } = this.state;
+    const { title, storyline, imagePath, genre, rating, subtitle } = getDetails;
+
+    // Change the condition to check the state
+    if (loading === true) return <Loading />;
 
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
+        <p>{`Tile ${title}`}</p>
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
+        <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+        <Link to="/">VOLTAR</Link>
       </div>
     );
   }
 }
 
 export default MovieDetails;
+
+MovieDetails.propTypes = {
+  match: propTypes.shape({
+    params: propTypes.shape({
+      id: propTypes.number,
+    }),
+  }).isRequired,
+};
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import MovieCard from '../components/MovieCard';
-
+import { Loading } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
@@ -9,17 +10,33 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loading: true,
     };
+    this.getRequest = this.getRequest.bind(this);
+  }
+
+  componentDidMount() {
+    this.getRequest();
+  }
+
+  async getRequest() {
+    const requestMovie = await movieAPI.getMovie();
+    this.setState({
+      movies: requestMovie,
+      loading: false,
+    });
   }
 
   render() {
-    const { movies } = this.state;
+    const { movies, loading } = this.state;
 
     // Render Loading here if the request is still happening
 
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        {loading ? <Loading />
+          : movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
       </div>
     );
   }
